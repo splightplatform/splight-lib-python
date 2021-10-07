24fd9ce88379635f475ee0a27acc182f6100a560
Matias Silva <matias.silva@splight-ae.com>
2021-10-07 10:24:27 -0300

    Tag 0.0.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError: # for pip <= 9.0.3
 
 setup(
     name='splight-lib',
-    version='0.1.0',
+    version='0.0.1',
     author='Splight',
     author_email='matias.silva@splight-ae.com',
     packages=['splight_lib', 'splight_lib.tests'],
