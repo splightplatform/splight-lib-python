9d2cfcd4324e35e489856909426b5c55e794b98f
Matias Silva <matias.silva@splight-ae.com>
2021-10-07 11:47:23 -0300

    Version 0.0.1 with tests

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     version='0.0.1',
     author='Splight',
     author_email='matias.silva@splight-ae.com',
-    packages=['splight_lib', 'splight_lib.tests'],
+    packages=['splight_lib', 'splight_lib.connector'],
     scripts=[],
     url=None,
     license='LICENSE.txt',
