b8aceaf03345ad2b6d97d88bd73de68b9cd0dd6b
Matias Silva <matias.silva@splight-ae.com>
2021-10-13 17:48:12 -0300

    Settings.py

--- splight_lib/__init__.py
+++ splight_lib/__init__.py
@@ -2,5 +2,5 @@ import os
 import django
 
 if not os.environ.get('DJANGO_SETTINGS_MODULE', None):
-    os.environ['DJANGO_SETTINGS_MODULE'] = 'orm.settings'
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'splight_storage.settings'
     django.setup()
