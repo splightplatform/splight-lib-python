db4906dabd6a04f9b40342ffd89244efde29db50
mateo_ostorero <mateo.ostorero@splight-ae.com>
2021-10-19 09:59:58 -0300

    finished writing views, testing is needed

--- splight_storage/views.py
+++ splight_storage/views.py
@@ -24,8 +24,26 @@ class PowerTransformerViewSet(viewsets.ModelViewSet):
     serializer_class = PowerTransformerSerializer
 
 
+class GeneratingUnitViewSet(viewsets.ModelViewSet):
+    queryset = devices.GeneratingUnitAsset.objects.all()
+    serializer_class = GeneratingUnitSerializer
+
+
+class LoadViewSet(viewsets.ModelViewSet):
+    queryset = devices.LoadAsset.objects.all()
+    serializer_class = LoadSerializer
+
+
+class ShuntViewSet(viewsets.ModelViewSet):
+    queryset = devices.ShuntAsset.objects.all()
+    serializer_class = ShuntSerializer
+
+
 asset_router = routers.DefaultRouter()
 asset_router.register('buses', BusViewSet)
 asset_router.register('lines', LineViewSet)
 asset_router.register('switches', SwitchViewSet)
 asset_router.register('transformers', PowerTransformerViewSet)
+asset_router.register('generating_units', GeneratingUnitViewSet)
+asset_router.register('loads', LoadViewSet)
+asset_router.register('shunts', ShuntViewSet)
