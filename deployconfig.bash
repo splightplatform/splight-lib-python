31f4a422add1b878d5d242c040f9e3114a1e63ba
Thomas Vadora <thomas.vadora@splight-ae.com>
2021-10-21 08:24:51 -0500

    add all steps in same pipeline

--- deployconfig.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
- cat << EOF >> ~/.pypirc
- [distutils]
- index-servers=splightlib
- [splightlib]
- repository:https://splight.jfrog.io/artifactory/api/pypi/testjfrog-pypi
- username=thomas.vadora@splight-ae.com
- password=4bhy35L@Tha!!fRpV2N3_oL1
- EOF
