dbf870fb9cd37e7873db941b901e3b29fe24f9e2
Matias Silva <matias.silva@splight-ae.com>
2021-10-28 16:43:32 -0300

    Database defined with psql connector

--- splight_storage/settings.py
+++ splight_storage/settings.py
@@ -14,3 +14,17 @@ DATABASES = {
         "NAME": db_location
     }
 }
+
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_NAME", "postgres"),
+        "USER": os.getenv("POSTGRES_USER", "postgres"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "postgres"),
+        "HOST": os.getenv("POSTGRES_HOST", "db"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
+    }
+}
