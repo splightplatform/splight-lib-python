3278f4abcccee520e81e614c1f7114a900a752d2
mateo_ostorero <mateo.ostorero@splight-ae.com>
2021-11-09 13:10:24 -0300

    Added geopoint model

--- splight_storage/models/asset/__init__.py
+++ splight_storage/models/asset/__init__.py
@@ -3,20 +3,19 @@ from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from model_utils.managers import InheritanceManager
 from pandas.core.frame import DataFrame
+from splight_storage.models.geopoint import Geopoint
 from splight_storage.models.tag import Tag
 from splight_storage.models.tenant import TenantAwareModel
 
 
 class Asset(TenantAwareModel):
     objects = InheritanceManager()
-    external_id = models.CharField(max_length=100, blank=True, unique=True) # TODO this must not be empty
+    # TODO this must not be empty
+    external_id = models.CharField(max_length=100, blank=True, unique=True)
     id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    longitude = models.DecimalField(
-        max_digits=6, decimal_places=3, null=True, blank=True)
-    latitude = models.DecimalField(
-        max_digits=6, decimal_places=3, null=True, blank=True)
     tags = models.ManyToManyField(Tag, blank=True)
+    geopoints = models.ManyToManyField(Geopoint, blank=True)
     connector_id = models.PositiveIntegerField(blank=True, null=True)
     connector_type = models.ForeignKey(
         ContentType,
