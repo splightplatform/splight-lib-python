7474b2cc5c877203df5ba44e970b5032c296471f
mateo_ostorero <mateo.ostorero@splight-ae.com>
2021-11-18 10:45:44 -0300

    Fixed decimal error, updated lib

--- splight_storage/models/asset/sensors.py
+++ splight_storage/models/asset/sensors.py
@@ -6,5 +6,5 @@ from .devices import LineAsset
 class DonutSensorAsset(Asset):
     line = models.ForeignKey(
         LineAsset, related_name='donuts', on_delete=models.CASCADE)
-    angle = models.DecimalField(max_digits=4, decimal_places=2)
+    angle = models.DecimalField(max_digits=5, decimal_places=2)
     orientation = models.CharField(max_length=100)
