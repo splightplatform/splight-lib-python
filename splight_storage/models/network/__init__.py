1d929c64e512e79de56eab7d90ec361f460af895
mateo_ostorero <mateo.ostorero@splight-ae.com>
2021-11-19 11:46:12 -0300

    Network and OpenVPNNetwork models V.2

--- splight_storage/models/network/__init__.py
+++ splight_storage/models/network/__init__.py
@@ -7,7 +7,8 @@ class Network(TenantAwareModel):
     id = models.BigAutoField(primary_key=True)
     objects = InheritanceManager()
 
-    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100, default='')
+    auth_type = models.CharField(max_length=100, default='')
 
     class Meta:
         app_label = 'splight_storage'
