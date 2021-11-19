ec51ac6da5fa8ec37bf4320931c24cddf89c2d28
mateo_ostorero <mateo.ostorero@splight-ae.com>
2021-11-19 11:46:12 -0300

    Network and OpenVPNNetwork models V.2

--- splight_storage/models/network/vpns.py
+++ splight_storage/models/network/vpns.py
@@ -1,5 +1,9 @@
+from django.db import models
 from . import Network
 
 
-class OpenVPN(Network):
-    pass
+class OpenVPNNetwork(Network):
+    username = models.CharField(max_length=100, null=True, blank=True)
+    password = models.CharField(max_length=100, null=True, blank=True)
+
+    file = models.FileField(blank=True, null=True)
