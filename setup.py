34dd4565af4d146dabbd9e5bba2a04e9a1a70125
mateo_ostorero <mateo.ostorero@splight-ae.com>
2021-11-19 11:47:39 -0300

    new lib verison

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ dependency_links = [
 
 setup(
     name='splight-lib',
-    version='0.0.15',
+    version='0.0.16',
     author='Splight',
     author_email='factory@splight-ae.com',
     packages=find_packages(),
