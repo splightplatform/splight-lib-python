6a983a8a6d30e82bb1cba80c259f60122dd0814e
mateo_ostorero <mateo.ostorero@splight-ae.com>
2021-11-25 18:31:42 -0300

    Minor tweeks

--- splight_datalake/files/__init__.py
+++ splight_datalake/files/__init__.py
@@ -7,9 +7,9 @@ class FileManager:
     def __init__(self, *args, **kwargs):
         self.manager = managers[os.getenv('FILE_SOURCE')](*args, **kwargs)
 
-    def retrieve_file(self, file_name, path):
+    def retrieve_file(self, file_name=None, path=None):
         filepath = self.manager.retrieve_file(file_name, path)
-        return filepath
+        return filepath.replace('/', os.sep)
 
     def upload_file(self, filepath):
-        self.manager.upload_file(filepath)
+        self.manager.upload_file(filepath.replace(os.sep, '/'))
