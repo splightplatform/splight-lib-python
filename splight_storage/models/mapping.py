6d0845c954fefa3f51eeb6129370beaed550c374
ignacioFernandez1 <ignacio.fernandez@mi.unc.edu.ar>
2021-11-30 15:20:30 -0300

    mapping with inheritanceManager

--- splight_storage/models/mapping.py
+++ splight_storage/models/mapping.py
@@ -1,26 +1,26 @@
 from django.db import models
 from django.db.models.fields import Field
+from model_utils.managers import InheritanceManager
 from splight_storage.models.tenant import TenantAwareModel
 from splight_storage.models.asset import Asset
 from splight_storage.models.connector import Connector
 
 
-class FieldMapping(TenantAwareModel):
+class Mapping(TenantAwareModel):
     path = models.CharField(max_length=300)
+    connector = models.ForeignKey(Connector, on_delete=models.CASCADE)
+    objects = InheritanceManager()
+
+
+# field and asset are in ClientMapping beacuse 'unique_together' forces them to be local fields
+class ClientMapping(Mapping):
     field = models.CharField(max_length=20)
-
-    class Meta:
-        abstract = True
-
-
-class ClientMapping(FieldMapping):
-    connector = models.ForeignKey(Connector, related_name="client_mappings", on_delete=models.CASCADE)
-    asset = models.ForeignKey(Asset, related_name="client_mapping", on_delete=models.CASCADE)
+    asset = models.ForeignKey(Asset, related_name="client_mappings", on_delete=models.CASCADE)
 
     class Meta:
         unique_together = ("field", "asset",)
 
 
-class ServerMapping(FieldMapping):
-    connector = models.ForeignKey(Connector, related_name="server_mappings", on_delete=models.CASCADE)
+class ServerMapping(Mapping):
+    field = models.CharField(max_length=20)
     asset = models.ForeignKey(Asset, related_name="server_mappings", on_delete=models.CASCADE)
