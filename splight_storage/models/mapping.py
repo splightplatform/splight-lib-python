06c6b8f7081ed93007d4c93327f37685a3eab78f
ignacioFernandez1 <ignacio.fernandez@mi.unc.edu.ar>
2021-11-30 16:01:27 -0300

    added restriction to valid fields in the selected asset

--- splight_storage/models/mapping.py
+++ splight_storage/models/mapping.py
@@ -6,10 +6,26 @@ from splight_storage.models.asset import Asset
 from splight_storage.models.connector import Connector
 
 
+class InvalidFieldException(Exception):
+    def __init__(self, conflicts, *args, **kwargs) -> None:
+        self.conflicts = conflicts
+        super().__init__(*args, **kwargs)
+
+
 class Mapping(TenantAwareModel):
     path = models.CharField(max_length=300)
     connector = models.ForeignKey(Connector, on_delete=models.CASCADE)
     objects = InheritanceManager()
+
+    def save(self, *args, **kwargs):
+        try:
+            # checkin if field is valid
+            obj = Asset.objects.get_subclass(id=self.asset.id)
+            if self.field not in [f.name for f in obj.__class__._meta.fields]:
+                raise InvalidFieldException(self.field)
+        except AttributeError:
+            pass
+        super(Mapping, self).save(*args, **kwargs)
 
 
 # field and asset are in ClientMapping beacuse 'unique_together' forces them to be local fields
