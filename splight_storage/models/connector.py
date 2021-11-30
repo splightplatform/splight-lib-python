a914ef8323fe914cb7448a6d5664c6aa0051affd
ignacioFernandez1 <ignacio.fernandez@mi.unc.edu.ar>
2021-11-30 11:40:01 -0300

    using network model as foreingkey

--- splight_storage/models/connector.py
+++ splight_storage/models/connector.py
@@ -1,7 +1,8 @@
 from django.db import models
 from splight_storage.models.tenant import TenantAwareModel
+from splight_storage.models.network import Network
 
 
 class Connector(TenantAwareModel):
-    network = models.IntegerField()
+    network = models.ForeignKey(Network, related_name="connectors", on_delete=models.CASCADE)
     ip = models.GenericIPAddressField()
