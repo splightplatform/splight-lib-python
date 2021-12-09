324f5af8985707997bb05656108e5bfcdc35ffbf
Matias Silva <matias.silva@splight-ae.com>
2021-12-09 10:48:43 -0300

    Generic healthcheck and error handling

--- splight_lib/component/abstract.py
+++ splight_lib/component/abstract.py
@@ -1,11 +1,21 @@
 import logging
+import os
+import signal
+import time
 from typing import Optional
 from abc import ABCMeta, abstractmethod
 from threading import Thread
 
 
+class HealthCheckException(Exception):
+    pass
+
+
+
 class AbstractComponent(metaclass=ABCMeta):
     managed_class = None
+    healthcheck_key = None
+    healthcheck_interval = 5
     logger = logging.getLogger()
 
     def __init__(self, instance_id: str, environment: Optional[str] = None):
@@ -27,17 +37,26 @@ class AbstractComponent(metaclass=ABCMeta):
         pass
 
     def healthcheck_task(self) -> None:
-        pass
+        if self.healthcheck_key is None:
+            return
+        while True:
+            if os.system(f"pgrep {self.healthcheck_key}  > /dev/null 2>&1") != 0:
+                raise HealthCheckException(f"{self.healthcheck_key} process not alive")
+            time.sleep(self.healthcheck_interval)
 
-    def execute(self, ) -> None:
+    def execute(self) -> None:
         self.pre_execution()
 
         threads = [
             Thread(target=self.refresh_task),
             Thread(target=self.server_task),
-            Thread(target=self.healthcheck_task),
+            Thread(target=self.main_task),
         ]
         for thread in threads:
             thread.start()
 
-        self.main_task()
+        try:
+            self.healthcheck_task()
+        except Exception as e:
+            self.logger.exception(e)
+            os.kill(os.getpid(), signal.SIGTERM)
