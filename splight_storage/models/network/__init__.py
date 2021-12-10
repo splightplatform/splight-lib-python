1804eacb1fa690d4d577e3091ffd45677f734e79
Mateo Ostorero <mateo.ostorero@splight-ae.com>
2021-12-10 18:47:03 +0000

    Merged in feature/FAC-201-add-hodor-ip-field-to-network (pull request #31)
    Feature/FAC-201 add hodor ip field to network
    
    * Added hodor ip field to generic network
    
    * Changed hodor_ip to hodor_address, altered migrations
    
    * lib update
    
    * removed auth and auth type from Network model
    
    
    Approved-by: Matias Silva

--- splight_storage/models/network/__init__.py
+++ splight_storage/models/network/__init__.py
@@ -6,9 +6,7 @@ from splight_storage.models.tenant import TenantAwareModel
 class Network(TenantAwareModel):
     id = models.BigAutoField(primary_key=True)
     objects = InheritanceManager()
-
-    type = models.CharField(max_length=100, default='')
-    auth_type = models.CharField(max_length=100, default='')
+    hodor_address = models.CharField(max_length=100, blank=True, default='')
 
     class Meta:
         app_label = 'splight_storage'
