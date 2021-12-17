0e9c19af72d77dd99aec369b2549d250496a868c
Federico Gonzalez Kriegel <federico.kriegel@splight-ae.com>
2021-12-17 15:13:14 +0000

    Merged in feature/FAC-207-improve-healthcheck-at-least-wit (pull request #33)
    Feature/FAC-207 improve healthcheck at least wit
    
    * Merge branch 'feature/FAC-207-improve-healthcheck-at-least-wit' of bitbucket.org:dp-factory/splight-lib into feature/FAC-207-improve-healthcheck-at-least-wit
    
    * Change names
    
    * Update abstract class
    
    * Uses terminate instead of kill
    
    * Add default value
    
    * Fix naming and errors
    
    * ResultStoredThread class
    
    * Add while true to healthcheck task
    
    * Update healthcheck task
    
    * Add TaskManager class
    
    
    Approved-by: Matias Silva

--- splight_lib/component/abstract.py
+++ splight_lib/component/abstract.py
@@ -4,21 +4,20 @@ import signal
 import time
 from typing import Optional
 from abc import ABCMeta, abstractmethod
-from threading import Thread
+from splight_lib.task import TaskManager
 
 
 class HealthCheckException(Exception):
     pass
 
 
-
 class AbstractComponent(metaclass=ABCMeta):
     managed_class = None
-    healthcheck_key = None
     healthcheck_interval = 5
     logger = logging.getLogger()
 
     def __init__(self, instance_id: str, environment: Optional[str] = None):
+        self.task_manager = TaskManager()
         self.instance_id = instance_id
         self.environment = environment
         self.object = self.managed_class.objects.get(id=instance_id)
@@ -37,23 +36,18 @@ class AbstractComponent(metaclass=ABCMeta):
         pass
 
     def healthcheck_task(self) -> None:
-        if self.healthcheck_key is None:
-            return
         while True:
-            if os.system(f"pgrep {self.healthcheck_key}  > /dev/null 2>&1") != 0:
-                raise HealthCheckException(f"{self.healthcheck_key} process not alive")
+            if not self.tasks_manager.all_tasks_ok():
+                raise HealthCheckException(f"Some process is not running")
+
             time.sleep(self.healthcheck_interval)
 
     def execute(self) -> None:
         self.pre_execution()
 
-        threads = [
-            Thread(target=self.refresh_task),
-            Thread(target=self.server_task),
-            Thread(target=self.main_task),
-        ]
-        for thread in threads:
-            thread.start()
+        self.task_manager.start_thread(target=self.refresh_task)
+        self.task_manager.start_thread(target=self.server_task)
+        self.task_manager.start_thread(target=self.main_task)
 
         try:
             self.healthcheck_task()
---
+++ splight_lib/task.py
@@ -0,0 +1,74 @@
+from typing import List, Callable, Union, Dict, Tuple
+from threading import Thread as DefaultThread
+from subprocess import Popen as DefaultPopen
+from functools import wraps
+import atexit
+import sys
+
+
+class Empty(object):
+    pass
+
+
+class Thread(DefaultThread):
+    def __init__(self, target, *args, **kwargs) -> None:
+        target = self.store_result(target)
+        self.result = Empty()
+        super().__init__(target=target, *args, **kwargs)
+
+    def store_result(self, func: Callable) -> Callable:
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            self.result = func(*args, **kwargs)
+            return self.result
+        return wrapper
+
+    def exit_ok(self) -> bool:
+        return not isinstance(self.result, Empty)
+
+    def kill(self) -> None:
+        pass
+
+    def terminate(self) -> None:
+        pass
+
+
+class Popen(DefaultPopen):
+    def is_alive(self) -> bool:
+        return self.poll() is None
+
+    def exit_ok(self) -> bool:
+        return self.poll() == 0
+
+
+class TaskManager:
+    def __init__(self):
+        self.tasks: List[Union[Thread, Popen]] = []
+        self._register_exit_functions()
+
+    def _register_exit_functions(self) -> None:
+        excepthook = sys.excepthook
+
+        def wrap_excepthook(type, value, traceback):
+            self.terminate_all()
+            excepthook(type, value, traceback)
+
+        atexit.register(self.terminate_all)
+        sys.excepthook = wrap_excepthook
+
+    def terminate_all(self) -> None:
+        for p in self.tasks:
+            p.terminate()
+
+    def start_process(self, target: str, args: List[str] = []):
+        p: Popen = Popen([target] + args)
+        self.tasks.append(p)
+
+    def start_thread(self, target: Callable, args: Tuple = (), kwargs: Dict = {}):
+        t: Thread = Thread(target=target, args=args, kwargs=kwargs)
+        self.tasks.append(t)
+        t.start()
+
+    @property
+    def healthcheck(self):
+        return all([p.is_alive() or p.exit_ok() for p in self.tasks])
