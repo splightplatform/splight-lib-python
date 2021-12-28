f581a3f4745f3854d88dc178852b2b0abf5c637f
Matias Silva <matias.silva@splight-ae.com>
2021-12-28 15:43:36 -0300

    Deployment moved from splight_api

--- splight_storage/models/network/__init__.py
+++ splight_storage/models/network/__init__.py
@@ -8,7 +8,6 @@ from splight_storage.models.tenant import TenantAwareModel
 class Network(TenantAwareModel):
     id = models.BigAutoField(primary_key=True)
     objects = InheritanceManager()
-    hodor_address = models.CharField(max_length=100, blank=True, default='')
 
     class Meta:
         app_label = 'splight_storage'
