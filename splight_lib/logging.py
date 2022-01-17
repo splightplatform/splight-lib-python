75db330e3b643ac1cdf564554b17a362cadf2c57
Matias Silva <matias.silva@splight-ae.com>
2022-01-17 14:56:25 -0300

    Improve logging

--- splight_lib/logging.py
+++ splight_lib/logging.py
@@ -3,6 +3,7 @@ import os
 
 
 def getLogger(name=None):
-    level = logging.DEBUG if os.getenv('DEBUG', False) else logging.INFO
-    logging.basicConfig(format='%(levelname)s | Date-Time : %(asctime)s | File: %(filename)s | Function: %(funcname)s | Line No. : %(lineno)d - %(message)s', level=level)
+    level = os.getenv('LOGLEVEL', logging.DEBUG)
+    fmt = '%(levelname)s | %(asctime)s | %(filename)s:%(lineno)d | %(message)s'
+    logging.basicConfig(format=fmt, level=level)
     return logging.getLogger(name)
