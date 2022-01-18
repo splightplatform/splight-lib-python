d55ae5453565f8c5907be87666565271b5b4e644
Matias Silva <matias.silva@splight-ae.com>
2022-01-18 12:10:46 -0300

    Bugfix Asset.set() queue by tenant

--- splight_storage/models/asset/__init__.py
+++ splight_storage/models/asset/__init__.py
@@ -1,17 +1,18 @@
 from django.db import models
+from typing import Any
+
+from splight_lib import logging
 from splight_lib.communication import Message, Variable, ExternalCommunicationClient
 from splight_lib.datalake import DatalakeClient
 from splight_storage.models.asset.attribute import Attribute
 from splight_storage.models.asset.base_asset import BaseAsset
 from splight_storage.models.mapping import ValueMapping, ReferenceMapping, ServerMapping, ClientMapping
 
-from typing import Any
-
-
-EXTERNAL_QUEUE = ExternalCommunicationClient()
-
 
 class Asset(BaseAsset):
+    logger = logging.getLogger()
+
+
     def read(self, asset: BaseAsset, name: str) -> Any:
         # Find atributes
         try:
@@ -24,21 +25,21 @@ class Asset(BaseAsset):
             variable: Variable = Variable(asset_id=asset.id, field=name, args=dict())
             variable = self.dl_client.fetch_updates([variable])
             return variable[0].args["value"]
-        except Exception:
-            pass
+        except Exception as e:
+            self.logger.debug(str(e))
 
         # ReferenceMapping recursive read
         try:
             mapping = ReferenceMapping.objects.get(asset=asset, attribute=attribute)
             return self.read(asset=mapping.ref_asset, name=mapping.ref_attribute.name)
-        except ReferenceMapping.DoesNotExist:
-            pass
+        except ReferenceMapping.DoesNotExist as e:
+            self.logger.debug(str(e))
 
         # ValueMapping read
         try:
             return ValueMapping.objects.get(asset=asset, attribute=attribute).value
-        except ValueMapping.DoesNotExist:
-            pass
+        except ValueMapping.DoesNotExist as e:
+            self.logger.debug(str(e))
 
         return getattr(asset, name)
 
@@ -59,8 +60,7 @@ class Asset(BaseAsset):
             else:
                 raise e
 
-    @staticmethod
-    def write(asset, name: str, value: Any) -> None:
+    def write(self, asset, name: str, value: Any) -> None:
         # Find atributes
         try:
             attribute = Attribute.objects.get(name=name)
@@ -71,20 +71,25 @@ class Asset(BaseAsset):
         if ClientMapping.objects.filter(asset=asset, attribute=attribute).exists():
             variable: Variable = Variable(asset_id=asset.id, field=name, args=dict(value=value))
             msg: Message = Message(action="write", variables=[variable])
-            EXTERNAL_QUEUE.send(msg.dict())
+            self.q_client.send(msg.dict())
             return
 
         # ReferenceMapping recursive write
         try:
             mapping = ReferenceMapping.objects.get(asset=asset, attribute=attribute)
-            return Asset.write(asset=mapping.ref_asset, name=mapping.ref_attribute.name, value=value)
-        except ReferenceMapping.DoesNotExist:
-            pass
+            return self.write(asset=mapping.ref_asset, name=mapping.ref_attribute.name, value=value)
+        except ReferenceMapping.DoesNotExist as e:
+            self.logger.debug(str(e))
 
         raise AttributeError(f"Object '{asset}' has not attribute '{name}'")
 
     def set(self, name: str, value: Any) -> None:
-        return Asset.write(asset=self, name=name, value=value)
+        try:
+            tenant = self.tenant.org_id
+        except AttributeError:
+            tenant = "default"
+        self.q_client = ExternalCommunicationClient(tenant)
+        return self.write(asset=self, name=name, value=value)
 
 
 class Bus(Asset):
