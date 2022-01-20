6e36cb3fd7275d96664e240d0ce762f0784e485a
Matias Silva <matias.silva@splight-ae.com>
2022-01-20 11:25:09 -0300

    Datalake pipelines fetch_assets fix

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ dependency_links = [
 
 setup(
     name='splight-lib',
-    version='0.0.46',
+    version='0.0.47',
     author='Splight',
     author_email='factory@splight-ae.com',
     packages=find_packages(),
