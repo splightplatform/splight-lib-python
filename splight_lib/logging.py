9f315cc4072d59a3c07d415471ad20e92e9e4135
Federico Gonzalez Kriegel <federico.kriegel@splight-ae.com>
2022-01-31 15:02:39 +0000

    Merged in feature/Dump-logs-to-file (pull request #54)
    Add LOG_FILE env var to dump logs in file
    
    * Add LOG_FILE env var to dump logs in file
    
    * Update lib vercion
    
    
    Approved-by: Matias Silva

--- splight_lib/logging.py
+++ splight_lib/logging.py
@@ -4,6 +4,16 @@ import os
 
 def getLogger(name=None):
     level = os.getenv('LOGLEVEL', logging.DEBUG)
+    log_file = os.getenv('LOG_FILE', None)
     fmt = '%(levelname)s | %(asctime)s | %(filename)s:%(lineno)d | %(message)s'
-    logging.basicConfig(format=fmt, level=level)
+    config = {
+        'level': level,
+        'format': fmt,
+    }
+    if log_file:
+        config['filename'] = log_file
+        config['filemode'] = 'a'
+
+    logging.basicConfig(**config)
+
     return logging.getLogger(name)
