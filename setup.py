22a1827f730c368a5119722396a11d75b7f6e7e0
Matias Silva <matias.silva@splight-ae.com>
2022-02-02 14:46:37 -0300

    Upgrade lib

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ dependency_links = [
 
 setup(
     name='splight-lib',
-    version='0.1.6',
+    version='0.1.7',
     author='Splight',
     author_email='factory@splight-ae.com',
     packages=find_packages(),
