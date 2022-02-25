733a028ffc5e2412f5a10f4d6c85a0538af37beb
Matias Silva <matias.silva@splight-ae.com>
2022-02-25 13:33:13 -0500

    Bugfix. Database selector

--- splight_database/django/settings.py
+++ splight_database/django/settings.py
@@ -7,12 +7,27 @@ INSTALLED_APPS = [
     'splight_database.django.djatabase'
 ]
 
-TESTING = "test" in sys.argv
-MEDIA_ROOT = "./media/"
+TESTING = "test" in sys.argv or "pytest" in sys.argv
+
+MEDIA_ROOT = os.path.join(os.getenv("HOME"), "media/")
+if not os.path.exists(MEDIA_ROOT):
+    os.makedirs(MEDIA_ROOT)
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.getenv("SQLITE_PATH", "./db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_NAME", "postgres"),
+        "USER": os.getenv("POSTGRES_USER", "postgres"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "postgres"),
+        "HOST": os.getenv("POSTGRES_HOST", "postgres"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
     }
 }
+
+if TESTING or os.getenv("DATABASE_TYPE", "SQLITE") == "SQLITE":
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.getenv("SQLITE_PATH", "./db.sqlite3"),
+        }
+    }
