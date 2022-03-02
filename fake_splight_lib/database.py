b71128eb11a3895611b9d429d6bcccbae8b46012
Federico Gonzalez Kriegel <federico.kriege@splight-ae.com>
2022-03-02 16:50:40 -0300

    Add filter in fake clients

--- fake_splight_lib/database.py
+++ fake_splight_lib/database.py
@@ -5,30 +5,27 @@ from splight_lib import logging
 from typing import Dict
 from splight_models import *
 from collections import defaultdict
+from client import validate_instance_type, validate_resource_type
 
 logger = logging.getLogger()
 
 
 class FakeDatabaseClient(AbstractDatabaseClient):
     database: Dict[Type, List[BaseModel]] = defaultdict(list)
-    CLASSES = [Network, Namespace, ClientMapping, ServerMapping, ValueMapping, ReferenceMapping, ClientConnector, ServerConnector, Asset, Attribute, Trigger, TriggerGroup, Tag]
+    valid_classes = [Network, Namespace, ClientMapping, ServerMapping, ValueMapping, ReferenceMapping, ClientConnector, ServerConnector, Asset, Attribute, Trigger, TriggerGroup, Tag]
 
     def _create(self, instance: BaseModel) -> BaseModel:
-        if type(instance) not in self.CLASSES:
-            raise NotImplementedError
-        
-        instance.id = str(len(self.database[type(instance)])+1)
+        instance.id = str(len(self.database[type(instance)]) + 1)
         self.database[type(instance)].append(instance)
         return instance
 
-
+    @validate_instance_type
     def save(self, instance: BaseModel) -> BaseModel:
         logger.debug(f"[FAKED] Executing save with {instance}")
-        if type(instance) not in self.CLASSES:
-            raise NotImplementedError
-        
+
         if type(instance) in [ClientMapping, ValueMapping, ReferenceMapping]:
-            same_mapping = lambda x : x.id != instance.id and x.asset_id == instance.asset_id and x.attribute_id == instance.attribute_id
+            def same_mapping(x):
+                return x.id != instance.id and x.asset_id == instance.asset_id and x.attribute_id == instance.attribute_id
             client_mappings = list(filter(lambda x: same_mapping(x), self.database[ClientMapping]))
             value_mappings = list(filter(lambda x: same_mapping(x), self.database[ValueMapping]))
             reference_mappings = list(filter(lambda x: same_mapping(x), self.database[ReferenceMapping]))
@@ -41,12 +38,9 @@ class FakeDatabaseClient(AbstractDatabaseClient):
                 return instance
         return self._create(instance)
 
-
+    @validate_resource_type
     def get(self, resource_type: Type, first=False, **kwargs) -> List[BaseModel]:
         logger.debug(f"[FAKED] Executing get with {resource_type}")
-        if resource_type not in self.CLASSES:
-            raise NotImplementedError
-
         queryset = self.database[resource_type]
         kwargs = self._validated_kwargs(resource_type, **kwargs)
         queryset = self._filter(queryset, **kwargs)
@@ -54,7 +48,7 @@ class FakeDatabaseClient(AbstractDatabaseClient):
             return queryset[0] if queryset else None
         return queryset
 
-
+    @validate_resource_type
     def delete(self, resource_type: Type, id: str) -> None:
         logger.debug(f"[FAKED] Executing delete with {resource_type} {id}")
         if resource_type not in self.CLASSES:
@@ -65,4 +59,3 @@ class FakeDatabaseClient(AbstractDatabaseClient):
             if item.id == id:
                 del queryset[i]
                 return
-        
