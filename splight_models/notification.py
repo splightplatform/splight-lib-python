71c2820a3ba881d83f9c100d693308c3c287aba3
Mariano Piatti <mariano.piatti@splight-ae.com>
2022-03-04 12:12:55 -0300

    Modifications to Notifications model

--- splight_models/notification.py
+++ splight_models/notification.py
@@ -4,7 +4,6 @@ from datetime import datetime
 
 class Notification(BaseModel):
     trigger_id: str
-    priority: str
-    message: str
-    value: float
+    update_value: float
     update_timestamp: datetime
+    timestamp: Optional[datetime]
