162c44388e8270fcdb8ef6659e6cca6858bb5532
Ignacio Laquiz <ignacio.laquiz@splight-ae.com>
2022-03-10 15:26:30 +0000

    Merged in FAC-331 (pull request #83)
    FAC-331: Allow null description in tags
    
    * FAC-331: Allow null description in tags
    
    
    Approved-by: Matias Silva

--- splight_database/django/djatabase/models/tag.py
+++ splight_database/django/djatabase/models/tag.py
@@ -4,4 +4,4 @@ from .namespace import NamespaceAwareModel
 
 class Tag(NamespaceAwareModel):
     name = models.CharField(max_length=100)
-    description = models.CharField(max_length=100, blank=True)
+    description = models.CharField(max_length=100, null=True, blank=True)
