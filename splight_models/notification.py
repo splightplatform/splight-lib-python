752605d712070005f172ec7fe7a69ec7518a0ed7
Mariano Piatti <mariano.piatti@splight-ae.com>
2022-03-11 18:57:13 +0000

    Merged in modifications-for-search-endpoint (pull request #84)
    Modifications for search endpoint
    
    * Add changes for new datalake client
    
    * Add changes for search endpoint
    
    * Solve conflicts
    
    
    Approved-by: Federico Gonzalez Kriegel

--- splight_models/notification.py
+++ splight_models/notification.py
@@ -3,6 +3,11 @@ from typing import Optional
 from datetime import datetime
 
 class Notification(BaseModel):
+    # TODO: "General" notifications here
+    pass
+
+
+class TriggerNotification(BaseModel):
     trigger_id: str
     update_value: float
     update_timestamp: datetime
