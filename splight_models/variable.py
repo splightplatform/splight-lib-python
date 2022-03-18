fbd45aec684bf246f1cbde6df97967250589ded3
Matias Silva <matias.silva@splight-ae.com>
2022-03-18 12:21:49 -0300

    Fix Variable with timestamp

--- splight_models/variable.py
+++ splight_models/variable.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from pydantic import BaseModel
 from typing import Dict, Optional
 
@@ -7,3 +8,4 @@ class Variable(BaseModel):
     path: Optional[str] = None
     asset_id: Optional[str] = None
     attribute_id: Optional[str] = None
+    timestamp: Optional[datetime] = None
