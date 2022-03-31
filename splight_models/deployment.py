bcd044d245353a68b9a5af98a9f1b55209dbcd08
Matias Silva <matias.silva@splight-ae.com>
2022-03-31 10:44:57 -0300

    Deployment fix

--- splight_models/deployment.py
+++ splight_models/deployment.py
@@ -6,11 +6,6 @@ class Deployment(BaseModel):
     id: Optional[str] = None
     type: str # eg. ClientConnector, Runner
     external_id: str = None # eg. 1
-    name: Optional[str] = None # eg. Forecasting, NetworkManager
-    version: str # eg. 0_2
+    version: str # eg. Forecasting-0_2
     parameters: List[Parameter] = []
     namespace: Optional[str] = None
-
-    @property
-    def hub_identifier(self):
-        return f"{self.name}-{self.version}"
