58340ad6b63f3aee84d475bab987abb8ed9451d1
Matias Silva <matias.silva@splight-ae.com>
2022-04-01 13:49:21 +0000

    Merged in FAC-336_b (pull request #98)
    Dashboard chartitem
    
    * Dashboard chartitem
    
    * Dashboard chartitem
    
    
    Approved-by: Ignacio Laquiz

--- splight_database/django/djatabase/models/dashboard.py
+++ splight_database/django/djatabase/models/dashboard.py
@@ -4,27 +4,32 @@ from .namespace import NamespaceAwareModel
 
 class Dashboard(NamespaceAwareModel):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=10, null=True, blank=True)
+    name = models.CharField(max_length=100, null=True, blank=True)
     description = models.CharField(max_length=100, null=True, blank=True)
 
 
 class Tab(NamespaceAwareModel):
     id = models.AutoField(primary_key=True)
-    dashboard = models.ForeignKey(Dashboard, on_delete=models.CASCADE, related_name="solution_tabs")
-    name = models.CharField(max_length=10, null=True, blank=True)
-
-
-class Filter(NamespaceAwareModel):
-    id = models.AutoField(primary_key=True)
-    operator = models.CharField(max_length=10, null=True, blank=True)
-    key = models.CharField(max_length=10, null=True, blank=True)
-    value = models.CharField(max_length=10, null=True, blank=True)
+    dashboard = models.ForeignKey(Dashboard, on_delete=models.CASCADE, related_name="dashboard_tabs")
+    name = models.CharField(max_length=100, null=True, blank=True)
 
 
 class Chart(NamespaceAwareModel):
     id = models.AutoField(primary_key=True)
     tab = models.ForeignKey(Tab, on_delete=models.CASCADE, related_name="tab_charts")
-    type = models.CharField(max_length=10, null=True, blank=True)
-    filters = models.ManyToManyField(Filter)
-    refresh_interval = models.CharField(max_length=10, null=True, blank=True)
-    relative_window_time = models.CharField(max_length=10, null=True, blank=True)
+    type = models.CharField(max_length=100, null=True, blank=True)
+    refresh_interval = models.CharField(max_length=100, null=True, blank=True)
+    relative_window_time = models.CharField(max_length=100, null=True, blank=True)
+
+
+class Filter(NamespaceAwareModel):
+    id = models.AutoField(primary_key=True)
+    chart_item = models.ForeignKey(Chart, on_delete=models.CASCADE, related_name="filters", null=True)
+    operator = models.CharField(max_length=100, null=True, blank=True)
+    key = models.CharField(max_length=100, null=True, blank=True)
+    value = models.CharField(max_length=100, null=True, blank=True)
+
+
+class ChartItem(NamespaceAwareModel):
+    id = models.AutoField(primary_key=True)
+    chart = models.ForeignKey(Chart, on_delete=models.CASCADE, related_name="chart_items")
--- splight_models/dashboard.py
+++ splight_models/dashboard.py
@@ -16,15 +16,21 @@ class Tab(BaseModel):
 
 class Filter(BaseModel):
     id: Optional[str]
+    chart_item_id: str
     operator: str = "eq"  # eg. eq, gt, gte, lt
     key: str              # asset_id
     value: str            # 5
+
+
+class ChartItem(BaseModel):
+    id: Optional[str]
+    chart_id: str
 
 
 class Chart(BaseModel):
     id: Optional[str]
     tab_id: str
     type: str
-    filters: List[Filter] = []
     refresh_interval: Optional[str] = None
     relative_window_time: Optional[str] = None
+
