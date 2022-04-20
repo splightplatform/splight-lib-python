e6c4540a9c553109435deb21b73476cc85a45f15
Mariano Piatti <mariano.piatti@splight-ae.com>
2022-04-20 14:54:51 +0000

    Merged in external-communication-fix-and-service-account-role-binding-on-namespace-creation (pull request #113)
    External communication fix and service account role binding on namespace creation
    
    * Changes to external communication client and namespace deployment
    
    * Update lib version
    
    * Add files
    
    
    Approved-by: Matias Silva

--- fake_splight_lib/communication.py
+++ fake_splight_lib/communication.py
@@ -1,5 +1,6 @@
 import logging
 from splight_communication.abstract import AbstractCommunication
+from datetime import date, datetime
 
 
 from queue import Queue
@@ -8,15 +9,22 @@ import json
 QUEUE = Queue()
 
 
+def json_serial(obj):
+    """JSON serializer for objects not serializable by default json code"""
+
+    if isinstance(obj, (datetime, date)):
+        return obj.isoformat()
+    raise TypeError ("Type %s not serializable" % type(obj))
+
 class FakeQueueCommunication(AbstractCommunication):
     logger = logging.getLogger()
 
     def __init__(self, *args, **kwargs):
-        pass
+        self.topic = "default"
 
     def send(self, data: dict) -> None:
         self.logger.debug(f"FakeQueueCommunication Sent data: {data}")
-        QUEUE.put(json.dumps(data))
+        QUEUE.put(json.dumps(data, default=json_serial))
 
     def receive(self) -> dict:
         data = json.loads(QUEUE.get())
