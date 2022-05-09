7553cb837a9d0c76fd32b305bd25ba486c55b4b3
Matias Silva <matias.silva@splight-ae.com>
2022-05-09 16:04:10 -0300

    Notify hotfix

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ dependency_links = [
 
 setup(
     name='splight-lib',
-    version='0.3.13',
+    version='0.3.14',
     author='Splight',
     author_email='factory@splight-ae.com',
     packages=find_packages(),
