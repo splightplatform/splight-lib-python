b1a60477f7675cf95995c5e87979321ff84b9c9f
Matias Silva <matias.silva@splight-ae.com>
2022-05-12 17:58:33 +0000

    Merged in feature/hubclient (pull request #129)
    HubClient
    
    * HubClient
    
    * HubClient
    
    * Bugfix
    
    
    Approved-by: Mariano Piatti
    Approved-by: Federico Gonzalez Kriegel

---
+++ splight_hub/splight.py
@@ -0,0 +1,38 @@
+import requests
+from client import validate_resource_type
+from pydantic import BaseModel
+from typing import List, Type
+from splight_models.runner import Algorithm, Network, Connector
+
+from splight_hub.abstract import AbstractHubClient
+from splight_hub.settings import SPLIGHT_HUB_HOST, SPLIGHT_HUB_TOKEN
+
+
+class SplightHubClient(AbstractHubClient):
+    valid_classes = [Algorithm, Network, Connector]
+    
+    def __init__(self, *args, **kwargs) -> None:
+        super(SplightHubClient, self).__init__(*args, **kwargs)
+        self.host = SPLIGHT_HUB_HOST
+        self.headers = {}
+        if SPLIGHT_HUB_TOKEN:
+            self.headers = {'Authorization': f'Token {SPLIGHT_HUB_TOKEN}'}
+
+    def save(self, instance: BaseModel) -> BaseModel:
+        raise NotImplementedError
+
+    @validate_resource_type
+    def get(self, resource_type: Type, first=False, **kwargs) -> List[BaseModel]:
+        url = "/".join([self.host, "hub", resource_type.__name__.lower()])
+        response = requests.get(url, headers=self.headers)
+        assert response.status_code == 200, "Unreachable hub host"
+        queryset = [
+            resource_type(**v)
+            for v in response.json()['results']
+        ]
+        kwargs = self._validated_kwargs(resource_type, **kwargs)
+        queryset = self._filter(queryset, **kwargs)
+        return queryset
+
+    def delete(self, resource_type: Type, id: str) -> None:
+        raise NotImplementedError
