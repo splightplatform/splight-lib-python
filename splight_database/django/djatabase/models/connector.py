7895c011a3065d46639e7958958c9650972547d7
Mariano Piatti <mariano.piatti@splight-ae.com>
2022-05-13 20:07:24 +0000

    Merged in readme_url_in_models (pull request #130)
    add readme_url to db fields
    
    * add readme_url to db fields
    
    * update lib version

--- splight_database/django/djatabase/models/connector.py
+++ splight_database/django/djatabase/models/connector.py
@@ -8,3 +8,4 @@ class Connector(NamespaceAwareModel):
     description = models.CharField(max_length=100, null=True, blank=True)
     version = models.CharField(max_length=100, null=True, blank=True)
     parameters = models.JSONField(default=dict)
+    readme_url = models.URLField(max_length=200, null=True, blank=True)
