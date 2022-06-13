259f0f20ea0af72148e8f55e18119ddb62b2ffb8
Ignacio Fernandez <ignacio.fernandez@mi.unc.edu.ar>
2022-06-13 11:23:25 -0300

    creating index on algorithm initialization

--- splight_lib/component/algorithms.py
+++ splight_lib/component/algorithms.py
@@ -6,5 +6,6 @@ from splight_models import Algorithm
 class AbstractAlgorithmComponent(AbstractComponent):
     managed_class = Algorithm
 
-    # def __init__(self, *args, **kwargs):
-    #     super(AbstractAlgorithmComponent, self).__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super(AbstractAlgorithmComponent, self).__init__(*args, **kwargs)
+        self.datalake_client.create_index(self.collection_name, [('asset_id', 1), ('timestamp', -1)])
