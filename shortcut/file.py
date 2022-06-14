0b417812416f015b73419e1d9fae0b083688578e
Mateo-os <54186929+Mateo-os@users.noreply.github.com>
2022-06-14 18:05:51 -0300

    Feature/shortcut save file (#23)
    * added shortcut function
    
    * updated lib
    
    * minor renaming
    
    * added shortcut function
    
    * minor renaming
    
    * updated lib version
    
    * added typing to functions, updates save file functions
    
    * changed collection name
    
    * added QOL function to s3 client
    
    * minor bug
    
    * bug-fixes
    
    * bug-fixes
    
    * added changes in compliance with pull reqquest
    
    Co-authored-by: mateo_ostorero <mateo.ostorero@splight-ae.com>

---
+++ shortcut/file.py
@@ -0,0 +1,32 @@
+from logging import raiseExceptions
+import os
+
+
+from typing import List, Any, Union, Optional
+from splight_lib.communication import Variable, Message, Action, ExternalCommunicationClient
+from splight_lib.datalake import DatalakeClient
+from splight_lib.storage import StorageClient, StorageFile
+from splight_models import *
+from splight_lib import logging
+
+logger = logging.getLogger()
+
+COLLECTION = 'files'
+
+
+def save_file(storage_client: StorageClient, datalake_client: DatalakeClient,
+              filename, prefix: Optional[str] = None,
+              asset_id: Optional[str] = None, attribute_id: Optional[str] = None, path: str = '', args: Dict = None) -> StorageFile:
+    if not os.path.exists(filename):
+        logger.error("File must be locally found")
+        raise Exception()
+    args = args if args else dict()
+    if args.get('file_id',None) is not None:
+        msg = "Can't overwrite file_id"
+        logger.error(msg)
+        raise Exception(msg)
+    file = storage_client.save(StorageFile(file=filename,content_type='str'), prefix=prefix)
+    args['file_id'] = file.id
+    var = Variable(asset_id=asset_id, attribute_id=attribute_id, path=path, args=args)
+    datalake_client.save(Variable, instances=[var], collection=COLLECTION)
+    return file
