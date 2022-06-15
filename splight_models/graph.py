fbacedea8f37f4d682ceb450229ddc6c52a6fc3a
fedekriegel <30979327+fedekriegel@users.noreply.github.com>
2022-06-15 16:30:14 -0300

    Feature/graph (#28)
    * Add graph suport in database
    
    * Update version
    
    * Add description to asset
    
    * rename field
    
    * Fix required
    
    * Update migrations
    
    * Update  lib  version
    
    * Move exeption classes
    
    Co-authored-by: Federico Gonzalez Kriegel <federico.kriege@splight-ae.com>

---
+++ splight_models/graph.py
@@ -0,0 +1,24 @@
+from typing import List, Optional
+from .base import SplightBaseModel
+
+
+class Graph(SplightBaseModel):
+    id: Optional[str]
+    title: str
+    description: Optional[str] = None
+    locked: bool = False
+
+
+class Node(SplightBaseModel):
+    id: Optional[str]
+    type: str
+    graph_id: str
+    asset_id: str
+
+
+class Edge(SplightBaseModel):
+    id: Optional[str]
+    directed: bool
+    graph_id: str
+    source_id: str
+    target_id: str
