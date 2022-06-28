21e0369921dae10b2d2e9c25ad7e86ebf612e701
Matias Silva <matias.silva@splight-ae.com>
2022-06-28 17:24:17 -0300

    Fix VariableToDataframe

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ dependency_links = [
 
 setup(
     name='splight-lib',
-    version='0.3.42',
+    version='0.3.43',
     author='Splight',
     author_email='factory@splight-ae.com',
     packages=find_packages(),
