bc16c56408e4190288dd42584ced47051becdf14
marianogaragiola <marianogaragiola@gmail.com>
2022-07-15 11:45:42 -0300

    Add settings and default for blockchain module

---
+++ splight_blockchain/settings.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+from pydantic import BaseSettings
+
+
+class ProviderSchemas(str, Enum):
+    HTTP = "http"
+    HTTPS = "https"
+
+
+class SplightBlockchainConfig(BaseSettings):
+    """Configuration for the Splight Blockchai module."""
+    SCHEMA: ProviderSchemas = ProviderSchemas.HTTP
+    PROVIDER: str = "34.229.23.244"
+    PORT: int = 8545
+
+
+blockchain_config = SplightBlockchainConfig()
