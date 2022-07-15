488fd2071c9252e8245702f71bc3363b3c474a16
marianogaragiola <marianogaragiola@gmail.com>
2022-07-15 14:10:00 -0300

    Add exceptions files

---
+++ splight_blockchain/exceptions.py
@@ -0,0 +1,26 @@
+
+class ProviderConnectionError(Exception):
+    def __init__(self):
+        self._msg = "Unable to connect to provider"
+
+    def __str__(self) -> str:
+        return self._msg
+
+
+class TransactionNotAllowed(Exception):
+    def __init__(self, transaction_name: str):
+        self._msg = f"Transaction {transaction_name} is not allowed"
+
+    def __str__(self) -> str:
+        return self._msg
+
+
+class LoadContractError(Exception):
+    def __init__(self, address: str, contract_json: str):
+        self._msg = (
+            f"An error occurred loading contract {address} from "
+            f"file {contract_json}"
+        )
+
+    def __str__(self) -> str:
+        return self._msg
