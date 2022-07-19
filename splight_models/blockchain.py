76470168910e9ac142068221264d09934f1f508b
marianogaragiola <marianogaragiola@gmail.com>
2022-07-19 16:57:00 -0300

    add FunctionResponse model

--- splight_models/blockchain.py
+++ splight_models/blockchain.py
@@ -1,8 +1,13 @@
-from typing import Any, Dict
+from typing import Any, Dict, Union, List
 
 from .base import SplightBaseModel
 
 
 class SmartContract(SplightBaseModel):
     address: str
-    abi: Dict[str, Any]
+    abi: List[Dict[str, Any]]
+
+
+class FunctionResponse(SplightBaseModel):
+    name: str
+    value: Union[int, float, str]
