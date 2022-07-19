cd2db347b4d9a3e626ab54a82127e6cbb4980ee5
marianogaragiola <marianogaragiola@gmail.com>
2022-07-19 16:43:55 -0300

    complete abstract.py class

--- splight_blockchain/abstract.py
+++ splight_blockchain/abstract.py
@@ -1,15 +1,29 @@
 from abc import abstractmethod
 
+from hexbytes import HexBytes
 from web3.datastructures import AttributeDict
 
 from client import AbstractClient
 
 
 class BlockchainClient(AbstractClient):
+
+    @abstractmethod
+    def get_balance(self):
+        pass
+
     @abstractmethod
     def call(self, method: str, *args):
         pass
 
     @abstractmethod
     def transact(self, method: str, *args, **kwargs) -> AttributeDict:
+        pass
+
+    @abstractmethod
+    def get_transaction(self, tx_hast: HexBytes) -> AttributeDict:
+        pass
+
+    @abstractmethod
+    def get_transaction_receipt(self, tx_hast: HexBytes) -> AttributeDict:
         pass
