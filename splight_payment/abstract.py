51873912290b6de1c806c1a125274f158282f644
Thomas Vadora <thvadora@gmail.com>
2022-07-18 22:09:54 -0300

    resolve comments, make customer_id an input in init

--- splight_payment/abstract.py
+++ splight_payment/abstract.py
@@ -6,26 +6,26 @@ class AbstractPaymentClient(AbstractClient):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
     
-    @abstractmethod
+    @staticmethod
     def create_customer(self, name: str, city: str, country: str, postal_code: str, email: str):
         pass
 
     @abstractmethod
-    def add_item(self, customer_id:str, description: str, price: float):
+    def add_item(self, description: str, price: float):
         pass
     
     @abstractmethod
-    def close(self, customer_id: str):
+    def close(self):
         pass
     
     @abstractmethod 
-    def add_card_to_customer(self, customer_id: str):
+    def add_card_to_customer(self):
         pass
     
     @abstractmethod
-    def modify_invoice_email(self, customer_id: str, email: str):
+    def modify_invoice_email(self, email: str):
         pass
     
     @abstractmethod
-    def get_invoices(self, customer_id: str):
+    def get_invoices(self):
         pass
--- splight_payment/stripe.py
+++ splight_payment/stripe.py
@@ -4,10 +4,12 @@ import stripe
 
 class StripeClient(AbstractPaymentClient):
 
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self, billing_id: str, *args, **kwargs) -> None:
         super(StripeClient, self).__init__(*args, **kwargs)
         stripe.api_key = STRIPE_APIKEY
+        self.customer_id = billing_id
 
+    @staticmethod
     def create_customer(self, name: str, city: str, country: str, postal_code: str, email: str):
         response = stripe.Customer.create(
             name=name, 
@@ -16,17 +18,17 @@ class StripeClient(AbstractPaymentClient):
         )
         return response['id']
 
-    def add_item(self, customer_id: str, description: str, price: float):
+    def add_item(self, description: str, price: float):
         ghost_product = stripe.Price.create(unit_amount=int(price*100), currency='usd', product_data={'name': description})
         ghost_product_id = ghost_product['id']
-        invoice_item = stripe.InvoiceItem.create(customer=customer_id, price= ghost_product_id)
+        invoice_item = stripe.InvoiceItem.create(customer=self.customer_id, price= ghost_product_id)
 
-    def close(self, customer_id: str):
-        invoice = stripe.Invoice.create(customer=customer_id, collection_method='send_invoice', days_until_due=7)
+    def close(self):
+        invoice = stripe.Invoice.create(customer=self.customer_id, collection_method='send_invoice', days_until_due=7)
         stripe.Invoice.finalize_invoice(invoice['id'])
     
-    def get_invoices(self, customer_id: str):
-        invoice_list = stripe.Invoice.list(customer=customer_id, limit=100)['data']
+    def get_invoices(self):
+        invoice_list = stripe.Invoice.list(customer=self.customer_id, limit=100)['data']
         response = []
         for invoice in invoice_list:
             filtered_invoice = {
@@ -39,13 +41,13 @@ class StripeClient(AbstractPaymentClient):
         return response
 
         
-    def add_card_to_customer(self, customer_id: str):
+    def add_card_to_customer(self):
         # not implementing right now
         pass
     
 
-    def modify_invoice_email(self, customer_id: str, email: str):
+    def modify_invoice_email(self, email: str):
         stripe.Customer.modify(
-            customer=customer_id,
+            customer=self.customer_id,
             email=email
         )
