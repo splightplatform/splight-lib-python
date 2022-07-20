5be1968206b492565459939bc907c49f0da44295
Matias Silva <matias.silva@splight-ae.com>
2022-07-20 08:51:48 -0300

    Rename mode

--- splight_database/django/djatabase/models/blockchain.py
+++ splight_database/django/djatabase/models/blockchain.py
@@ -7,7 +7,7 @@ class BlockchainContract(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True)
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=100, null=True, blank=True)
-    account_id = models.CharField(max_length=100, null=True, blank=True)
+    address = models.CharField(max_length=100, null=True, blank=True)
     abi_json = models.JSONField(default=dict)
 
     def to_dict(self):
