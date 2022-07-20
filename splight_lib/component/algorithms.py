6b39f612cd43b1e335d6fce7535ecf35899fcce8
diegolupi93 <emanuel.lupi91@gmail.com>
2022-07-20 10:57:33 -0300

    remove useless check.

--- splight_lib/component/algorithms.py
+++ splight_lib/component/algorithms.py
@@ -12,9 +12,7 @@ class AbstractAlgorithmComponent(AbstractComponent):
         self.datalake_client.create_index(self.collection_name, [('attribute_id', 1), ('asset_id', 1), ('timestamp', -1)])
 
     def get_algorithm_result_history(self, collection_name: str, **kwargs) -> VariableDataFrame:
-        if collection_name not in self.instance.sub_algorithms:
-            raise ValueError(f'collection {collection_name} is not a {self.collection_name} sub algorithms')
-        
+
         return self.datalake_client.get_dataframe(
             resource_type=Variable,
             collection=collection_name,
