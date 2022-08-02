6b63db89d1d313aaac9ae042e1a134f91c25c6be
Mariano Garagiola <marianogaragiola@gmail.com>
2022-08-02 11:24:09 -0300

    Feature/auth manager (#74)
    * add pydantic model for users
    
    * add auth manager to interact with auth api
    
    * use AbstractClient
    
    * remove commented code
    
    * add endpoints for auth client
    
    * add fake auth client
    
    * selector for auth client
    
    * Add docstrings
    
    * Fix wrong http methods calls
    
    * add unittest for auth module
    
    * add missing requirement
    
    * update version
    
    * fix typo
    
    * set organization endpoint as retriever
    
    * Add missing / for base url
    
    * add user activity models
    
    * fix strings
    
    * fix imports for running unittest
    
    * remove raise for status after requests
    
    * set version

--- splight_models/base.py
+++ splight_models/base.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 
+
 class SplightBaseModel(BaseModel):
     def __hash__(self):  # make hashable BaseModel subclass
         return hash(self.__class__.__name__ + str(tuple(self.dict().values())))
