a2f2ce2f4269ad185b69c1f36be88c68009880c6
marianogaragiola <marianogaragiola@gmail.com>
2022-08-05 10:58:52 -0300

    complete abstract interface for auth

--- splight_abstract/auth/abstract.py
+++ splight_abstract/auth/abstract.py
@@ -2,6 +2,36 @@ from abc import ABC, abstractmethod
 
 
 class AbstractAuthClient(ABC):
+    @property
+    @abstractmethod
+    def credentials(self):
+        pass
+
+    @property
+    @abstractmethod
+    def profile(self):
+        pass
+
+    @property
+    @abstractmethod
+    def role(self):
+        pass
+
+    @property
+    @abstractmethod
+    def user(self):
+        pass
+
+    @property
+    @abstractmethod
+    def organization(self):
+        pass
+
+    @property
+    @abstractmethod
+    def superadmin(self):
+        pass
+
     @abstractmethod
     def authenticate(self):
         pass
