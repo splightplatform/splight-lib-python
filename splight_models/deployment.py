a4cba45939efaad3b31069c1aa425c2eda37e82c
diegolupi93 <emanuel.lupi91@gmail.com>
2022-08-08 11:56:05 -0300

    refactor component size options.

--- splight_models/deployment.py
+++ splight_models/deployment.py
@@ -1,6 +1,14 @@
 from .base import SplightBaseModel
 from typing import Optional, List
 from splight_models.runner import Parameter
+from enum import Enum
+
+
+class ComponentSize(str, Enum):
+    small_component_size = 'small'
+    medium_component_size = 'medium'
+    large_component_size = 'large'
+    very_large_component_size = 'very_large'
 
 
 class Deployment(SplightBaseModel):
@@ -10,4 +18,4 @@ class Deployment(SplightBaseModel):
     version: str  # eg. Forecasting-0_2
     parameters: List[Parameter] = []
     namespace: Optional[str] = None
-    component_capacity: int
+    component_capacity: Optional[str] = ComponentSize.medium_component_size
