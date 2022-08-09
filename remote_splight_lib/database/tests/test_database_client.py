175e95e5e76bbd2c89e5d6614cb742cb655cad51
marianogaragiola <marianogaragiola@gmail.com>
2022-08-09 17:41:28 -0300

    fix test

--- remote_splight_lib/database/tests/test_database_client.py
+++ remote_splight_lib/database/tests/test_database_client.py
@@ -1,9 +1,12 @@
+from pytest import MonkeyPatch
+MonkeyPatch().setenv("SPLIGHT_ACCESS_KEY", "access_key")
+MonkeyPatch().setenv("SPLIGHT_SECRET_KEY", "secret_key")
+
 from unittest import TestCase
 from unittest.mock import patch
 
-from remote_splight_lib.auth import SplightAuthToken
 from remote_splight_lib.database import DatabaseClient
-from remote_splight_lib.database.client import Session
+from remote_splight_lib.database.client import Session, settings
 from splight_models import Attribute
 
 ATTR_ID = "fb3ae667-aa77-4064-b7b4-8181e0fd5477"
@@ -45,7 +48,7 @@ class TestDatabaseClient(TestCase):
         return client
 
     @patch.object(
-        Session, "post", autospect=True, return_value=MockAttributeResponse()
+        Session, "post", autospec=True, return_value=MockAttributeResponse()
     )
     def test_create_new_resource(self, mocked_method):
         client = self.get_client()
