4c17d547a55d191cbc0c1dff323a525386ec107c
diegolupi93 <emanuel.lupi91@gmail.com>
2022-08-11 08:57:32 -0300

    Loggin level and restart policies.

--- splight_models/deployment.py
+++ splight_models/deployment.py
@@ -1,14 +1,30 @@
+import logging
 from .base import SplightBaseModel
 from typing import Optional, List
 from splight_models.runner import Parameter
 from enum import Enum
 
 
-class ComponentSize(Enum):
+class ComponentSize(str, Enum):
     small = 'small'
     medium = 'medium'
     large = 'large'
     very_large = 'very_large'
+
+
+class RestartPolicy(str, Enum):
+    never = 'Never'
+    on_failure = 'OnFailure'
+    always = 'Always'
+
+
+class LogginLevel(str, Enum):
+    critical = logging.CRITICAL
+    error = logging.ERROR
+    warning = logging.WARNING
+    info = logging.INFO
+    debug = logging.DEBUG
+    notset = logging.NOTSET
 
 
 class Deployment(SplightBaseModel):
@@ -18,4 +34,6 @@ class Deployment(SplightBaseModel):
     version: str  # eg. Forecasting-0_2
     parameters: List[Parameter] = []
     namespace: Optional[str] = None
-    component_capacity: ComponentSize = ComponentSize.medium
+    component_capacity: str = ComponentSize.medium.value
+    log_level: int = LogginLevel.debug.value
+    restart_policy: str = RestartPolicy.always.value
