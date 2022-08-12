901ebcf90e3826c04812bb215432d540319b513d
marianogaragiola <marianogaragiola@gmail.com>
2022-08-12 11:22:17 -0300

    update fake notification client

--- fake_splight_lib/notification.py
+++ fake_splight_lib/notification.py
@@ -1,7 +1,8 @@
-from splight_abstract import AbstractNotificationClient
-from splight_models import Notification
-from splight_lib import logging
+from typing import Dict
 
+from splight_abstract import AbstractNotificationClient
+from splight_lib import logging
+from splight_models import Notification
 
 logger = logging.getLogger()
 
@@ -11,5 +12,10 @@ class FakeNotificationClient(AbstractNotificationClient):
     def channel(self):
         return f"private-{self.namespace}"
 
-    def send(self, instance: Notification, topic: str = 'default') -> None:
-        logger.info(f"[FAKED] Notification sent channel:{self.channel} topic:{topic} data:{instance.dict()} ")
+    def send(self, instance: Notification, topic: str = "default") -> None:
+        logger.info(
+            f"[FAKED] Notification sent channel:{self.channel} topic:{topic} data:{instance.dict()} "
+        )
+
+    def authenticate(self, channel_name: str, socket_id: str) -> Dict:
+        return {"auth": "1234"}
