2363e63325243a457802b8abcaa41fd15237f9a8
marianogaragiola <marianogaragiola@gmail.com>
2022-08-12 11:15:36 -0300

    update abstract class

--- splight_abstract/notification/abstract.py
+++ splight_abstract/notification/abstract.py
@@ -1,9 +1,16 @@
 from abc import abstractmethod
+from typing import Dict
 from splight_models import Notification
 from splight_abstract.client import AbstractClient
+
 
 class AbstractNotificationClient(AbstractClient):
 
     @abstractmethod
     def send(self, instance: Notification, topic: str = 'default') -> None:
-        self._client.trigger(self.channel, topic, instance.dict())
+        pass
+        # self._client.trigger(self.channel, topic, instance.dict())
+
+    @abstractmethod
+    def authenticate(self, channel_name: str, socket_id: str) -> Dict:
+        pass
