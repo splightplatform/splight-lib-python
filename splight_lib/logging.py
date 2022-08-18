45aaa508b0d2102b33636a8fc54a8d919693519a
Mariano Piatti <mariano.piatti@splight-ae.com>
2022-08-18 17:42:59 -0300

    change debug level

--- splight_lib/logging.py
+++ splight_lib/logging.py
@@ -3,7 +3,7 @@ import os
 
 
 def getLogger(name=None):
-    level = os.getenv('LOG_LEVEL', logging.INFO)
+    level = os.getenv('LOG_LEVEL', logging.DEBUG)
     log_file = os.getenv('LOG_FILE', None)
     fmt = '%(levelname)s | %(asctime)s | %(filename)s:%(lineno)d | %(message)s'
     config = {
