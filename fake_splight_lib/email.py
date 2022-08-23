4f3ac655107d4611a25e13fde5646c699d236da0
Mariano Piatti <mariano.piatti@splight-ae.com>
2022-08-23 11:43:58 -0300

    add mailing

---
+++ fake_splight_lib/email.py
@@ -0,0 +1,13 @@
+from splight_abstract import AbstractEmailClient
+from typing import List, Union
+from splight_models import EmailType
+from splight_lib import logging
+
+logger = logging.getLogger()
+
+class FakeEmailClient(AbstractEmailClient):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def send(self, subject: str, message: str, to: Union[str, List[str]], type: EmailType = EmailType.INFO):
+        logger.info(f"[FAKED] Email sent to {to}")
---
+++ splight_abstract/email/abstract.py
@@ -0,0 +1,12 @@
+from abc import abstractmethod
+from splight_abstract.client import AbstractClient
+from typing import List, Union
+from splight_models import EmailType
+
+class AbstractEmailClient(AbstractClient):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @abstractmethod
+    def send(self, subject: str, message: str, to: Union[str, List[str]], type: EmailType = EmailType.INFO):
+        pass
