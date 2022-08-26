090fd8881f3b6a59db3a1703cbadeb0279068e5e
Mariano Piatti <mariano.piatti@splight-ae.com>
2022-08-26 11:49:04 -0300

    Add contact form

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ dependency_links = [
 
 setup(
     name='splight-lib',
-    version='0.4.14',
+    version='0.4.15',
     author='Splight',
     author_email='factory@splight-ae.com',
     packages=find_packages(),
