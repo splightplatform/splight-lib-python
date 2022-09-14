69d1e4ea0bea194be47f834c7bdd3c6e9a7d7368
Emanuel Lupi <emanuel.lupi@splight-ae.com>
2022-09-14 12:34:43 -0300

    remove stripe from lib.

--- remote_splight_lib/auth/exceptions.py
+++ remote_splight_lib/auth/exceptions.py
@@ -1,14 +1,3 @@
-class AuthenticationError(Exception):
-    def __init__(self, url: str, status_code: int):
-        self._msg = (
-            f"An error ocurred during authentication with status "
-            f"code {status_code}"
-        )
-
-    def __str__(self) -> str:
-        return self._msg
-
-
 class SignatureVerificationError(Exception):
     def __init__(self, message, sig_header, http_body=None):
         super(SignatureVerificationError, self).__init__(message)
