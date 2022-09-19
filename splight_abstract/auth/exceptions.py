f8129026b42210183c61d4ee82c6b7eb3ca6b455
marianogaragiola <marianogaragiola@gmail.com>
2022-09-19 17:24:03 -0300

    minor update in exception

--- splight_abstract/auth/exceptions.py
+++ splight_abstract/auth/exceptions.py
@@ -1,9 +1,6 @@
 class AuthenticationError(Exception):
-    def __init__(self, url: str, status_code: int):
-        self._msg = (
-            f"An error ocurred during authentication with status "
-            f"code {status_code}"
-        )
+    def __init__(self, url: str):
+        self._msg = "An error ocurred during authentication"
 
     def __str__(self) -> str:
         return self._msg
