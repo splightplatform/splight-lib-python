586649674de22c71e29dc08bdb034895376c7218
marianogaragiola <marianogaragiola@gmail.com>
2022-09-19 17:23:46 -0300

    add model for credentials

---
+++ splight_models/credential.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from typing import Optional
+
+from splight_models.base import SplightBaseModel
+
+
+class Credential(SplightBaseModel):
+    access_id: str
+    secret_key: Optional[str] = None
+    created_date: Optional[datetime] = None
+    last_used: Optional[datetime] = None
+    user: int
+    description: Optional[str] = None
