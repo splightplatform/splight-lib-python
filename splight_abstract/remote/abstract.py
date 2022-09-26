2a2b4587b1842d967afd65d0d5a161c2e4c4e063
Ignacio Fernandez <nachofernandez@Ignacios-MacBook-Pro.local>
2022-09-26 15:17:58 -0300

    apply feedback

--- splight_abstract/remote/abstract.py
+++ splight_abstract/remote/abstract.py
@@ -3,13 +3,10 @@ from splight_abstract.client import AbstractClient
 
 
 class AbstractRemoteClient(AbstractClient):
-    list_type_trailings = ("__in", )
-
     def _parse_params(self, **kwargs):
         params = {}
         for key, value in kwargs.items():
-            if key.endswith(self.list_type_trailings):
+            params[key] = value
+            if isinstance(value, list):
                 params[key] = ','.join(value)
-            else:
-                params[key] = value
         return params
