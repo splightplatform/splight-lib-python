a1cf35e2c744e330af6b025bde86e333041b6463
Mariano Piatti <mariano.piatti@splight-ae.com>
2022-09-26 14:29:04 -0300

    add changes to email client

--- splight_abstract/email/abstract.py
+++ splight_abstract/email/abstract.py
@@ -1,6 +1,6 @@
 from abc import abstractmethod
 from splight_abstract.client import AbstractClient
-from typing import List, Union
+from typing import List, Union, Dict, Optional
 from splight_models import EmailType
 
 class AbstractEmailClient(AbstractClient):
@@ -8,9 +8,17 @@ class AbstractEmailClient(AbstractClient):
         super().__init__(*args, **kwargs)
 
     @abstractmethod
-    def send(self, subject: str, message: str, to: Union[str, List[str]], type: EmailType = EmailType.INFO):
+    def send(self, subject: str, to: Union[str, List[str]], variables: Dict, template: Optional[str] = None, type: EmailType = EmailType.INFO):
+        pass
+
+    @abstractmethod
+    def send_template_to_all_organizations(self, name: str, template_id: str, unsubscribe_group_id: int) -> None:
         pass
 
     @abstractmethod
     def add_to_newsletter(self, email: str) -> None:
+        pass
+
+    @abstractmethod
+    def add_organization_contact(self, org_data: Dict) -> None:
         pass
