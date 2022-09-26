96e152ef15e455b653ead5432e8fe6402ffef590
Mariano Piatti <mariano.piatti@splight-ae.com>
2022-09-26 15:33:51 -0300

    apply feedback

--- fake_splight_lib/email.py
+++ fake_splight_lib/email.py
@@ -1,5 +1,6 @@
 from splight_abstract import AbstractEmailClient
 from typing import List, Union, Dict, Optional
+from datetime import timedelta
 from splight_models import EmailType
 from splight_lib import logging
 
@@ -12,11 +13,8 @@ class FakeEmailClient(AbstractEmailClient):
     def send(self, subject: str, to: Union[str, List[str]], variables: Dict, template: Optional[str] = None, type: EmailType = EmailType.INFO):
         logger.info(f"[FAKED] Email sent to {to}")
 
-    def send_template_to_all_organizations(self, name: str, template_id: str, unsubscribe_group_id: int) -> None:
+    def send_to_list(self, name: str, template_id: str, group_id: str, unsubscribe_group_id: int, delay: Optional[timedelta] = None) -> None:
         logger.info(f"[FAKED] Sent template {template_id} to all organizations")
 
-    def add_to_newsletter(self, email: str) -> None:
-        logger.info(f"[FAKED] Added {email} to newsletter")
-
-    def add_organization_contact(self, org_data: Dict) -> None:
-        logger.info(f"[FAKED] Added {org_data} to organization contacts")
+    def add_contact_to_list(self, list_id: str, data: Dict) -> None:
+        logger.info(f"[FAKED] Added contact to list {list_id}")
