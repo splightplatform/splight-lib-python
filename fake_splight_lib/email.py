83f17287ff7695d4f8083487a0394df993937d7f
Mariano Piatti <mariano.piatti@splight-ae.com>
2022-09-26 14:29:04 -0300

    add changes to email client

--- fake_splight_lib/email.py
+++ fake_splight_lib/email.py
@@ -1,5 +1,5 @@
 from splight_abstract import AbstractEmailClient
-from typing import List, Union
+from typing import List, Union, Dict, Optional
 from splight_models import EmailType
 from splight_lib import logging
 
@@ -9,8 +9,14 @@ class FakeEmailClient(AbstractEmailClient):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def send(self, subject: str, message: str, to: Union[str, List[str]], type: EmailType = EmailType.INFO):
+    def send(self, subject: str, to: Union[str, List[str]], variables: Dict, template: Optional[str] = None, type: EmailType = EmailType.INFO):
         logger.info(f"[FAKED] Email sent to {to}")
+
+    def send_template_to_all_organizations(self, name: str, template_id: str, unsubscribe_group_id: int) -> None:
+        logger.info(f"[FAKED] Sent template {template_id} to all organizations")
 
     def add_to_newsletter(self, email: str) -> None:
         logger.info(f"[FAKED] Added {email} to newsletter")
+
+    def add_organization_contact(self, org_data: Dict) -> None:
+        logger.info(f"[FAKED] Added {org_data} to organization contacts")
