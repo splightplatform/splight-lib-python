cfa23671dc28bc2b799fefeb79f767f37adfa79d
Matias Silva <matias.silva@splight-ae.com>
2022-10-04 17:11:25 -0300

    Readme and picture as hubcomponents fields

--- splight_models/hub.py
+++ splight_models/hub.py
@@ -1,6 +1,6 @@
 from typing import Optional
 from pydantic import validator
-from .runner import BaseRunner
+from splight_models.runner import BaseRunner
 from typing import List
 
 VERIFICATION_CHOICES = ['verified', 'unverified', 'official']
@@ -13,8 +13,8 @@ class HubComponent(BaseRunner):
     description: Optional[str]
     privacy_policy: Optional[str] = None
     tenant: Optional[str] = None
-    readme_url: Optional[str]
-    picture_url: Optional[str]
+    readme: Optional[str]
+    picture: Optional[str]
     verification: Optional[str]
     created_at: Optional[str]
     last_modified: Optional[str]
