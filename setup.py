bab513f8bbbe97e490ec4bba451b5673428b589b
Matias Silva <matisilva18@hotmail.com>
2022-10-12 12:18:28 -0500

    Readme and picture as hubcomponents fields (#135)
    * Readme and picture as hubcomponents fields
    
    * Lib upgrade
    
    Co-authored-by: Matias Silva <matias.silva@splight-ae.com>

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ dependency_links = [
 
 setup(
     name='splight-lib',
-    version='0.4.39',
+    version='0.4.40',
     author='Splight',
     author_email='factory@splight-ae.com',
     packages=find_packages(),
