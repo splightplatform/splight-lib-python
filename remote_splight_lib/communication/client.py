78b1b25a9430ee8b7122f7f0aa9747c37711bcf6
Ignacio Fernandez <nachofernandez@Ignacios-MacBook-Pro.local>
2022-10-17 12:02:43 -0300

    suggestiongs

--- remote_splight_lib/communication/client.py
+++ remote_splight_lib/communication/client.py
@@ -48,7 +48,7 @@ class CommunicationFactory:
 
 class CommunicationClient(AbstractCommunicationClient):
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, daemon: bool = True, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._status = CommunicationClientStatus.STOPPED
         self._channel_bindings = []
@@ -58,8 +58,7 @@ class CommunicationClient(AbstractCommunicationClient):
             ("pusher:connection_failed", self.__on_connection_failed),
             ("pusher:error", self.__on_error),
         ]
-        # Filter client kwargs
-        self._client_kwargs = {k: v for k, v in kwargs.items() if k in ['daemon']}
+        self._daemon = daemon
         try:
             self.__load_context()
             self.__load_client()
@@ -96,7 +95,7 @@ class CommunicationClient(AbstractCommunicationClient):
             auth_endpoint=self._context.auth_endpoint,
             auth_endpoint_headers=self._context.auth_headers,
             user_data=self._context.channel_data.dict() if self.context.channel_data else None,
-            **self._client_kwargs
+            daemon=self._daemon,
         )
         self.__bind_system_events()
         self._client.connect()
