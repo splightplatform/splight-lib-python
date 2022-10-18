a3abf6ee5b0c410637aae2447cd0a5909d9194b0
Mateo-Os <mateoostorero71@gmail.com>
2022-10-18 15:52:55 -0300

    updated lib vesion

--- splight_models/ingestion.py
+++ splight_models/ingestion.py
@@ -1,8 +1,12 @@
 from .base import SplightBaseModel
 from typing import Optional
+from pydantic import Field
+from datetime import datetime,timezone
+
 
 class Ingestion(SplightBaseModel):
-  name: str
-  description: Optional[str]
   id: Optional[str]
-
+  asset_id: Optional[str] = None
+  name: str
+  description: Optional[str] = None
+  timestamp: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
