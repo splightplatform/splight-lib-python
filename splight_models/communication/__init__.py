6eb8aeaa91f75f941c95cf86da5e5ce7ed4d625b
Matias Silva <matisilva18@hotmail.com>
2022-10-25 15:06:54 -0300

    Matisilva/operate refactor (#153)
    * Reorder and naming
    
    * Reorder and naming
    
    * Fix tests
    
    * Refactor load_parameters for input to be used by operation too
    
    * Refactor Operation OperationResponse models
    
    * Bugfix callback
    
    * Update splight_lib/component/abstract.py
    
    Co-authored-by: Mariano Piatti <30485597+mariano-piatti@users.noreply.github.com>
    
    * Bugfix multiple and SIMPLE_TYPES
    
    * Changed event
    
    * Fixed custom_types and database_type
    
    Co-authored-by: Matias Silva <matias.silva@splight-ae.com>
    Co-authored-by: Mariano Piatti <30485597+mariano-piatti@users.noreply.github.com>

---
+++ splight_models/communication/__init__.py
@@ -0,0 +1,17 @@
+from splight_models.communication.models import *
+from splight_models.communication.events import *
+
+
+__all__ = [
+    "Operation", 
+    "CommunicationChannelData",
+    "CommunicationClientStatus",
+    "CommunicationContext",
+    "CommunicationEvent",
+    "OperationCreateEvent",
+    "OperationUpdateEvent",
+    "MappingCreateEvent",
+    "MappingDeleteEvent",
+    "RuleCreateEvent",
+    "RuleDeleteEvent",
+]
---
+++ splight_models/communication/events.py
@@ -0,0 +1,66 @@
+
+from enum import Enum
+from typing import Dict, Optional
+from pydantic import Field
+from splight_models.base import SplightBaseModel
+from splight_models.communication import Operation
+from splight_models.mapping import ClientMapping
+from splight_models.rule import MappingRule
+from splight_models.user import User
+from datetime import datetime, timezone
+
+
+class EventNames(str, Enum):
+    OPERATION_TRIGGER = 'operation-trigger'
+    OPERATION_CREATE = 'operation-create'
+    OPERATION_UPDATE = 'operation-update'
+    MAPPING_CREATE = "mapping-create"
+    MAPPING_DELETE = "mapping-delete"
+    RULE_CREATE = 'rule-create'
+    RULE_DELETE = 'rule-delete'
+
+
+class CommunicationEvent(SplightBaseModel):
+    event_name: str
+    id: Optional[str] = None
+    instance_id: Optional[str] = None
+    socket_id: Optional[str] = None
+    timestamp: str = Field(default_factory=lambda: datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%S.%fZ"))  # pusher cannot json serialize datetime objects
+    display_text: Optional[str] = None
+    user: Optional[User] = None
+    data: Dict
+
+
+class OperationTriggerEvent(CommunicationEvent):
+    event_name: str = Field(EventNames.OPERATION_TRIGGER, const=True)
+    data: Operation
+
+
+class OperationCreateEvent(CommunicationEvent):
+    event_name: str = Field(EventNames.OPERATION_CREATE, const=True)
+    data: Operation
+
+
+class OperationUpdateEvent(CommunicationEvent):
+    event_name: str = Field(EventNames.OPERATION_UPDATE, const=True)
+    data: Operation
+
+
+class MappingCreateEvent(CommunicationEvent):
+    event_name: str = Field(EventNames.MAPPING_CREATE, const=True)
+    data: ClientMapping
+
+
+class MappingDeleteEvent(CommunicationEvent):
+    event_name: str = Field(EventNames.MAPPING_DELETE, const=True)
+    data: ClientMapping
+
+
+class RuleCreateEvent(CommunicationEvent):
+    event_name: str = Field(EventNames.RULE_CREATE, const=True)
+    data: MappingRule
+
+
+class RuleDeleteEvent(CommunicationEvent):
+    event_name: str = Field(EventNames.RULE_DELETE, const=True)
+    data: MappingRule
