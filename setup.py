01b3960001630a98e082ebfc5f38b93c6f2d986b
marianogaragiola <marianogaragiola@gmail.com>
2022-11-09 10:04:25 -0300

    update library version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ dependency_links = [
 
 setup(
     name='splight-lib',
-    version='0.4.55',
+    version='0.4.56',
     author='Splight',
     author_email='factory@splight-ae.com',
     packages=find_packages(),
