5a82e18d67670c956ab2bf9f8ae42b4926bc4f24
Mariano Piatti <mariano.piatti@splight-ae.com>
2022-11-09 17:33:08 -0300

    fix tests

--- splight_lib/shortcut/__init__.py
+++ splight_lib/shortcut/__init__.py
@@ -2,7 +2,6 @@ from splight_lib.shortcut.notification import notify
 from splight_lib.shortcut.file import save_file
 from splight_lib.shortcut.organization import OrganizationHandler
 
-
 __all__ = [
     notify,
     save_file,
--- splight_lib/tests/test_shortcut.py
+++ splight_lib/tests/test_shortcut.py
@@ -4,7 +4,6 @@ from unittest import TestCase
 class TestShortcut(TestCase):
     def test_import_shortcut(self):
         from splight_lib.shortcut import (
-            rule_eval,
             notify,
             save_file,
             OrganizationHandler,
