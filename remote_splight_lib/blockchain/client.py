6cddb1c80b8a6c17feea2760e18967aef66b477b
Victor Martini <victor.cesar.m@gmail.com>
2022-11-11 09:30:23 -0300

    Send transact request body as json

--- remote_splight_lib/blockchain/client.py
+++ remote_splight_lib/blockchain/client.py
@@ -27,7 +27,7 @@ class BlockchainClient(AbstractBlockchainClient, AbstractRemoteClient):
         # POST /blockchain/contract/{contract.address}/transact/{method}
         url = self._base_url / f"{self._PREFIX}/contract/{contract_address}/transact/{method}/"
         data = {"private_key": private_key, **kwargs}
-        response = self._session.post(url, data=data)
+        response = self._session.post(url, json=data)
         response.raise_for_status()
         return response
 
