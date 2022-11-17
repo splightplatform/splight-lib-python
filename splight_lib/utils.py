b15a94689bbffef63316fae1e2fdddbd322ee2ec
marianogaragiola <marianogaragiola@gmail.com>
2022-11-17 19:13:18 -0300

    add config object for encrypting values

--- splight_lib/utils.py
+++ splight_lib/utils.py
@@ -1,11 +1,13 @@
+from pydantic import BaseSettings
 from cryptography.fernet import Fernet
-from splight_lib.settings import setup
 
 
-ENCRYPTION_KEY = setup.settings.SPLIGHT_ENCRYPTION_KEY
+class EncryptionSettings(BaseSettings):
+    SPLIGHT_ENCRYPTION_KEY: str
 
 
 def decrypt_value_validator(cls, value: str) -> str:
-    encrypter = Fernet(ENCRYPTION_KEY.encode("utf-8"))
+    config = EncryptionSettings()
+    encrypter = Fernet(config.SPLIGHT_ENCRYPTION_KEY.encode("utf-8"))
     decripted = encrypter.decrypt(value.encode("utf-8")).decode("utf-8")
     return decripted
