d9e33f70fee97080c327cdd93cb6bec495fdb353
marianogaragiola <marianogaragiola@gmail.com>
2022-11-18 17:51:36 -0300

    validator for decrypting value supports list

--- splight_lib/encryption.py
+++ splight_lib/encryption.py
@@ -1,17 +1,24 @@
-from typing import Union
+from typing import List, Union
 
-from pydantic import BaseSettings
 from cryptography.fernet import Fernet
+from pydantic import BaseSettings
 
 
 class EncryptionSettings(BaseSettings):
     SPLIGHT_ENCRYPTION_KEY: str
 
 
-def decrypt_value_validator(cls, value: str) -> str:
+def decrypt_value_validator(
+    cls, value: Union[str, List[str]]
+) -> Union[str, List[str]]:
     config = EncryptionSettings()
     encrypter = Fernet(config.SPLIGHT_ENCRYPTION_KEY.encode("utf-8"))
-    decripted = encrypter.decrypt(value.encode("utf-8")).decode("utf-8")
+    if isinstance(value, list):
+        decripted = [
+            encrypter.decrypt(v.encode("utf-8")).decode("utf-8") for v in value
+        ]
+    else:
+        decripted = encrypter.decrypt(value.encode("utf-8")).decode("utf-8")
     return decripted
 
 
