6802dcef4c5e9851cf7be6f3c2b5f941a79422bf
Federico Gonzalez Kriegel <federico.kriege@splight-ae.com>
2022-11-24 15:10:04 -0300

    Apply feedback

--- splight_models/file.py
+++ splight_models/file.py
@@ -1,7 +1,8 @@
-import os
-from typing import Optional, Dict
 from .base import SplightBaseModel
+from typing import Optional, Dict
+from pydantic import validator
 import json
+import os
 
 
 class File(SplightBaseModel):
@@ -12,12 +13,11 @@ class File(SplightBaseModel):
     content_type: Optional[str] = None
     url: Optional[str] = None
 
-    def __init__(self, **kwargs):
-        file = kwargs.pop('file', None)
-        file = file.replace('/', os.sep)
-        file = file.replace('\\', os.sep)
-        kwargs['file'] = file
-        super(File, self).__init__(**kwargs)
+    @validator("file", pre=True)
+    def validate_file(cls, v):
+        v = v.replace('/', os.sep)
+        v = v.replace('\\', os.sep)
+        return v
 
     @property
     def name(self):
