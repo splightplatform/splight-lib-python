8a721030eccf80733113a362d4af39866a87a815
Mariano Piatti <mariano.piatti@splight-ae.com>
2022-11-25 15:33:03 -0300

    apply feedback

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ dependency_links = [
 
 setup(
     name='splight-lib',
-    version='0.5.9',
+    version='0.5.10',
     author='Splight',
     author_email='factory@splight-ae.com',
     packages=find_packages(),
