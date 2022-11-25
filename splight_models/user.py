2fb610f0026f5e1d199d21750d2c0be9f046309f
Victor Martini <victor.cesar.m@gmail.com>
2022-11-25 18:49:12 -0300

    Update model

--- splight_models/user.py
+++ splight_models/user.py
@@ -54,7 +54,7 @@ class User(SplightBaseModel):
 
 class OrganizationMetadata(SplightBaseModel):
     billing_id: Optional[str] = None
-    payouts_id: Optional[str] = None
+    payout_account_id: Optional[str] = None
     referral_code: Optional[str] = None
     blockchain_id: Optional[str] = None
     manager_email: Optional[str] = None
