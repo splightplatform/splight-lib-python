52f25e78128f86a8d7150fffb73c6e8fc6e75d53
Matias Silva <matisilva18@hotmail.com>
2022-11-28 13:20:25 -0300

    Bind native objects (#186)
    * Added option to bind for native objects
    
    * Remove daemon=True in helthcheck task
    
    Co-authored-by: Matias Silva <matias.silva@splight-ae.com>
    Co-authored-by: Federico Gonzalez Kriegel <federico.kriege@splight-ae.com>

--- splight_models/base.py
+++ splight_models/base.py
@@ -4,3 +4,7 @@ from pydantic import BaseModel
 class SplightBaseModel(BaseModel):
     def __hash__(self):  # make hashable BaseModel subclass
         return hash(self.__class__.__name__ + str(tuple(self.dict().values())))
+
+    @staticmethod
+    def get_event_name(type: str, action: str) -> str:
+        return f"{type.lower()}_{action}"
