f87a4600e206f26e46daba5ec87cbfea636bb540
Victor Martini <victor.cesar.m@gmail.com>
2022-11-30 13:41:15 -0300

    Remove payout_id and referral_code from organization metadata serializer, now we have a model for that

--- splight_models/user.py
+++ splight_models/user.py
@@ -54,8 +54,6 @@ class User(SplightBaseModel):
 
 class OrganizationMetadata(SplightBaseModel):
     billing_id: Optional[str] = None
-    payout_account_id: Optional[str] = None
-    referral_code: Optional[str] = None
     blockchain_id: Optional[str] = None
     manager_email: Optional[str] = None
 
