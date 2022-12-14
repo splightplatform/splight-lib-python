9b22cf29ae3f30c4c9037377964059d71422eb03
Matias Silva <matisilva18@hotmail.com>
2022-12-14 14:23:49 -0300

    Lib upgrade: Query .source calculated (#197)
    Co-authored-by: Matias Silva <matias.silva@splight-ae.com>

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ dependency_links = [
 
 setup(
     name='splight-lib',
-    version='0.5.25',
+    version='0.5.26',
     author='Splight',
     author_email='factory@splight-ae.com',
     packages=find_packages(),
