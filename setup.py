afdcf5e9f6dd016fd4ce1b6c8ff4b10ccad22d93
marianogaragiola <marianogaragiola@gmail.com>
2022-12-21 12:23:22 -0300

    other fix

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ dependency_links = [
 
 setup(
     name='splight-lib',
-    version='0.5.35',
+    version='0.5.36',
     author='Splight',
     author_email='factory@splight-ae.com',
     packages=find_packages(),
