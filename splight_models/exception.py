0e3f9f4397cebe98f4a49423d548eaa656647049
marianogaragiola <marianogaragiola@gmail.com>
2022-12-28 17:19:03 -0300

    feat: add WebhookEvent model

--- splight_models/exception.py
+++ splight_models/exception.py
@@ -24,3 +24,11 @@ class LockedGraphException(Exception):
 
 class CrossGraphException(Exception):
     pass
+
+
+class InvalidModel(Exception):
+    def __init__(self, model_name: str):
+        self._msg = f"Model {model_name} is not a valid Splight model"
+
+    def __str__(self) -> str:
+        return self._msg
