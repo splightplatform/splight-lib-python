ff1a091b662064f1dbf172d341cf0409a5c2046b
Lautaro Lombardi <lautaro.lombardi@mi.unc.edu.ar>
2023-01-13 17:57:35 -0300

    prototype notification

--- splight_models/notification.py
+++ splight_models/notification.py
@@ -5,49 +5,29 @@ from enum import Enum
 from splight_models.datalake import DatalakeModel
 
 
-SYSTEM = 'system'
-INFO = 'info'
-LOW = 'low'
-MEDIUM = 'medium'
-HIGH = 'high'
-CRITICAL = 'critical'
-
-GREATER_THAN = 'gt'
-GREATER_THAN_OR_EQUAL = 'ge'
-LOWER_THAN = 'lt'
-LOWER_THAN_OR_EQUAL = 'le'
-EQUAL = 'eq'
-
-SEVERITIES = (
-    (SYSTEM, 'system'),
-    (INFO, 'info'),
-    (LOW, 'low'),
-    (MEDIUM, 'medium'),
-    (HIGH, 'high'),
-    (CRITICAL, 'critical'),
-)
-
-SOURCE_TYPE = (
-    ('Component', 'Component'),
-    ('Algorithm', 'Algorithm'),
-    ('Connector', 'Connector'),
-    ('Network', 'Network'),
-)
-
 class SourceType(str, Enum):
-    # TODO: Remove this once component types are unified
     component = 'Component'
-    algorithm = 'Algorithm'
-    network = 'Network'
-    connector = 'Connector'
+    system = 'System'
+    organization = 'Organization'
+    user = 'User'
+
+class TargetType(str, Enum):
+    component = 'Component'
+    organization = 'Organization'
+    asset = 'Asset'
+    attribute = 'Attribute'
+    file_ = 'File'
+    graph = 'Graph'
+    query = 'Query'
+    user = 'User'
 
 
 class Notification(DatalakeModel):
     id: Optional[str]
-    title: str
     message: str
     seen: bool = False
     timestamp: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
-    severity: str = INFO
     source_id: Optional[str]
     source_type: Optional[SourceType] = SourceType.component
+    target_id: Optional[str]
+    target_type: Optional[TargetType] = TargetType.component
