eb6ebb7ed83a0bb3024f83c6b9cfb40b68cc248d
Ignacio Fernandez <nachofernandez@Ignacios-MacBook-Pro.local>
2023-01-19 10:13:17 -0800

    new requirement cryptography

--- setup.py
+++ setup.py
@@ -10,7 +10,11 @@ dependency_links = [
 
 setup(
     name='splight-lib',
+<<<<<<< HEAD
     version='2.0.0',
+=======
+    version='0.6.4',
+>>>>>>> 7f89b66 (new requirement cryptography)
     author='Splight',
     author_email='factory@splight-ae.com',
     packages=find_packages(),
