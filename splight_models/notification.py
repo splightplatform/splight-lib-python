d70f4dcad47e57ccaadf93902fe7ac0925cc4368
Matias Silva <matias.silva@splight-ae.com>
2023-01-23 12:55:29 -0300

    Fix Notification model

--- splight_models/notification.py
+++ splight_models/notification.py
@@ -2,14 +2,14 @@ from pydantic import Field
 from datetime import datetime, timezone
 from typing import Optional
 from enum import Enum
-from splight_models.datalake import DatalakeModel
+from splight_models import SplightBaseModel
 
 
 class SourceType(str, Enum):
     component = 'Component'
     system = 'System'
-    organization = 'Organization'
     user = 'User'
+
 
 class TargetType(str, Enum):
     component = 'Component'
@@ -20,18 +20,19 @@ class TargetType(str, Enum):
     graph = 'Graph'
     query = 'Query'
 
-class Notification(DatalakeModel):
+
+class Notification(SplightBaseModel):
     id: Optional[str]
     message: str
     seen: bool = False
-    timestamp: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    created_at: str = Field(default_factory=lambda: datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%S.%fZ"))
     notify_by_email: Optional[bool] = False
     notify_by_web: Optional[bool] = True
     notify_by_sms: Optional[bool] = False
-    notify_by_push: Optional[bool] = False
-    source_id: Optional[str]
-    source_type: Optional[SourceType] = SourceType.component
-    target_id: Optional[str]
-    target_type: Optional[TargetType] = TargetType.component
+    notify_by_push: Optional[bool] = True
+    source_id: Optional[str] = None
+    source_type: Optional[SourceType] = None
+    target_id: Optional[str] = None
+    target_type: Optional[TargetType] = None
     volatile: bool = False
     redirect_url: Optional[str]
