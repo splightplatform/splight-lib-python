e0fa2b8f2996a4a4d916e2a90a44f605aef2e79a
marianogaragiola <marianogaragiola@gmail.com>
2023-02-14 08:47:53 -0300

    feat: add is_satisfied method for alert model

--- splight_models/alert.py
+++ splight_models/alert.py
@@ -1,17 +1,30 @@
-from typing import Optional
+import builtins
+import operator
+from typing import Optional, Union
 
 from pydantic import Field
+
 from splight_models.base import SplightBaseModel
-from splight_models.constants import SeverityType, AlertOperator
+from splight_models.constants import (
+    AlertOperator,
+    AlertVariableType,
+    SeverityType,
+)
 
 
 class Alert(SplightBaseModel):
     id: Optional[str] = Field(None, max_length=100)
     query_id: str = Field(..., max_length=100)
-    value: str
+    type: AlertVariableType = AlertVariableType.FLOAT
+    threshold: str
     message: str
     name: str
     description: Optional[str] = None
-    severity: SeverityType
-    operator: AlertOperator
+    severity: SeverityType = SeverityType.info
+    operator: AlertOperator = AlertOperator.EQUAL
     period: float = 1.0
+
+    def is_satisfied(self, value: Union[float, int, str]) -> bool:
+        rule_value = getattr(builtins, self.type)(self.threshold)
+        value = getattr(builtins, self.type)(value)
+        return getattr(operator, self.operator)(value, rule_value)
