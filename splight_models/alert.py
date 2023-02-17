a2f9f2249028d7c947e578ba42594eb1f2eaf35a
marianogaragiola <marianogaragiola@gmail.com>
2023-02-17 17:26:47 -0300

    feat: update models for alerts

--- splight_models/alert.py
+++ splight_models/alert.py
@@ -10,22 +10,16 @@ from .query import Query
 from splight_models.constants import (
     AlertOperator,
     AlertVariableType,
-    SeverityType,
+    # SeverityType,
 )
 
 
-class Alert(SplightBaseModel):
+class AlertCondition(SplightBaseModel):
     id: Optional[str] = Field(None, max_length=100)
-    query_id: str = Field(..., max_length=100)
-    query: Optional[Query] = None
-    type: AlertVariableType = AlertVariableType.FLOAT
-    threshold: str
-    message: str
-    name: str
-    description: Optional[str] = None
-    severity: SeverityType = SeverityType.info
+    query: Query
     operator: AlertOperator = AlertOperator.EQUAL
-    period: float = 1.0
+    threshold: str
+    type: AlertVariableType = AlertVariableType.FLOAT
     namespace: str = Field("default", alias="namespace_id")
     deleted: bool = False
 
@@ -33,7 +27,23 @@ class Alert(SplightBaseModel):
     def convert_to_str(cls, value: Union[str, UUID]):
         return str(value)
 
+
+class Alert(SplightBaseModel):
+    id: Optional[str] = Field(None, max_length=100)
+    message: str
+    name: str
+    description: Optional[str] = None
+    period: float = 1.0
+    namespace: str = Field("default", alias="namespace_id")
+    deleted: bool = False
+    condition: AlertCondition
+
+    @validator("id", pre=True)
+    def convert_to_str(cls, value: Union[str, UUID]):
+        return str(value)
+
     def is_satisfied(self, value: Union[float, int, str]) -> bool:
+        __import__('ipdb').set_trace()
         rule_value = getattr(builtins, self.type)(self.threshold)
         value = getattr(builtins, self.type)(value)
         return getattr(operator, self.operator)(value, rule_value)
