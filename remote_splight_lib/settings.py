29bb07c957b6275c836f95022a43d09e59031881
marianogaragiola <marianogaragiola@gmail.com>
2023-02-28 13:23:09 -0300

    add envs vars for remote clients settings

--- remote_splight_lib/settings.py
+++ remote_splight_lib/settings.py
@@ -1,10 +1,14 @@
-from pydantic import BaseSettings
+from pydantic import BaseSettings, Field
 
 
 class SplightRemoteSettings(BaseSettings):
     SPLIGHT_PLATFORM_API_HOST: str = "https://api.splight-ae.com"
-    SPLIGHT_ACCESS_ID: str
-    SPLIGHT_SECRET_KEY: str
+    SPLIGHT_ACCESS_ID: str = Field(
+        ..., env=["SPLIGHT_ACCESS_ID", "SPLIGHT_AUTH_ACCESS_ID"]
+    )
+    SPLIGHT_SECRET_KEY: str = Field(
+        ..., env=["SPLIGHT_SECRET_KEY", "SPLIGHT_AUTH_SECRET_KEY"]
+    )
 
 
 settings = SplightRemoteSettings()
