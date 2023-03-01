203ba9a757d550854a69166937de94ed6570d90a
acurto <agustin.curto@splight-ae.com>
2023-03-01 16:20:35 -0300

    Define Splight Logger extending logging.Logger

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ dependency_links = [
 
 setup(
     name='splight-lib',
-    version='2.0.24-dev0',
+    version='2.0.25-dev0',
     author='Splight',
     author_email='factory@splight-ae.com',
     packages=find_packages(),
