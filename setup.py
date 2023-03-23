4bb0f225751fdfd59f040d16566df2e6a1c629b7
Lautaro Lecumberry <lautarolecumberry@gmail.com>
2023-03-23 09:54:53 -0300

    update version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ dependency_links = [
 
 setup(
     name='splight-lib',
-    version='2.0.30',
+    version='2.0.35',
     author='Splight',
     author_email='factory@splight-ae.com',
     packages=find_packages(),
