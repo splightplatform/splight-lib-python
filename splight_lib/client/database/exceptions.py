7df2094f9662127043ff6a3928acf29b6eda4dbf
marianogaragiola <marianogaragiola@gmail.com>
2023-03-30 17:15:53 -0300

    move remote database client

--- splight_lib/client/database/exceptions.py
+++ splight_lib/client/database/exceptions.py
@@ -1,3 +1,10 @@
+class InvalidModel(Exception):
+    def __init__(self, model_name: str):
+        self._msg = f"Model {model_name} is not a valid database model"
+
+    def __str__(self) -> str:
+        return self._msg
+
 
 class InstanceNotFound(Exception):
     def __init__(self, name: str, uid: str):
