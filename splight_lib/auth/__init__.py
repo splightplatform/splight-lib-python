17bc6df965333292af50215a83db7b522b404524
marianogaragiola <marianogaragiola@gmail.com>
2023-03-30 17:16:31 -0300

    move splight auth token

--- splight_lib/auth/__init__.py
+++ splight_lib/auth/__init__.py
@@ -1,1 +1,7 @@
-from .mac_auth import HmacSignature
+from splight_lib.auth.mac_auth import HmacSignature
+from splight_lib.auth.token import SplightAuthToken
+
+__all__ = [
+    HmacSignature,
+    SplightAuthToken,
+]
