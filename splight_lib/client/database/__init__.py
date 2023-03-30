e334b86186676a483218c07aaab3f64828255638
marianogaragiola <marianogaragiola@gmail.com>
2023-03-30 17:15:53 -0300

    move remote database client

--- splight_lib/client/database/__init__.py
+++ splight_lib/client/database/__init__.py
@@ -1,8 +1,10 @@
 from splight_lib.client.database.local_client import LocalDatabaseClient
+from splight_lib.client.database.remote_client import RemoteDatabaseClient
 from splight_lib.client.database.builder import DatabaseClientBuilder
 
 
 __all__ = [
     LocalDatabaseClient,
+    RemoteDatabaseClient,
     DatabaseClientBuilder,
 ]
