43cbe7961cf0617eaecb6e2d6f7d61fbe366e814
marianogaragiola <marianogaragiola@gmail.com>
2023-03-31 14:42:11 -0300

    remove database client builder

--- splight_lib/client/database/__init__.py
+++ splight_lib/client/database/__init__.py
@@ -1,10 +1,8 @@
 from splight_lib.client.database.local_client import LocalDatabaseClient
 from splight_lib.client.database.remote_client import RemoteDatabaseClient
-from splight_lib.client.database.builder import DatabaseClientBuilder
 
 
 __all__ = [
     LocalDatabaseClient,
     RemoteDatabaseClient,
-    DatabaseClientBuilder,
 ]
