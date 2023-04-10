446a9982eb990ba05594559c42e22f59787bb81d
Agustin Curto <agustin.curto@splight-ai.com>
2023-04-10 12:20:14 -0300

    Force logging handlers override

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ dependency_links = [
 
 setup(
     name='splight-lib',
-    version='2.2.2',
+    version='2.2.3',
     author='Splight',
     author_email='factory@splight-ae.com',
     packages=find_packages(),
