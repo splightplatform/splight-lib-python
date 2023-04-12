dbc9e07773e49f5a66771293dfd985ed0b4da615
franco <franco@truecircle.ai>
2023-04-12 15:57:23 +0200

    removes GeometryCollection and adds geojson-pydantic

--- splight_models/__init__.py
+++ splight_models/__init__.py
@@ -8,7 +8,6 @@ from splight_models.credential import *
 from splight_models.datalake import *
 from splight_models.deployment import *
 from splight_models.file import *
-from splight_models.geometry import Geometry, GeometryCollection
 from splight_models.graph import *
 from splight_models.hub import *
 from splight_models.namespace import *
