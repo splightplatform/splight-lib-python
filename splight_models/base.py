f8034ae0dbc83005ee59c580e8d7ca3c3340ce2d
MarcoCBA <smartinellimarco@icloud.com>
2023-04-21 12:59:18 -0300

    Add classname method and output format from resource type.

--- splight_models/base.py
+++ splight_models/base.py
@@ -8,3 +8,12 @@ class SplightBaseModel(BaseModel):
     @staticmethod
     def get_event_name(type: str, action: str) -> str:
         return f"{type.lower()}-{action.lower()}"
+
+    @classmethod
+    def class_name(cls):
+        """Get class name of the model from the repr string
+        Class attribute is mapped to the metaclass.
+        """
+        clean_path = "".join([c for c in str(cls) if c.isalpha() or c =="."])
+        class_name = clean_path.split(".")[-1]
+        return class_name
