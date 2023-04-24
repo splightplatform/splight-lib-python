4843646ffdfc2f0e494c38f0a53d1c646eb3b93b
marianogaragiola <marianogaragiola@gmail.com>
2023-04-24 15:43:08 -0300

    feat: update database client with new interface

--- splight_lib/client/database/__init__.py
+++ splight_lib/client/database/__init__.py
@@ -1,7 +1,9 @@
+from splight_lib.client.database.builder import DatabaseClientBuilder
 from splight_lib.client.database.local_client import LocalDatabaseClient
 from splight_lib.client.database.remote_client import RemoteDatabaseClient
 
 __all__ = [
     LocalDatabaseClient,
+    DatabaseClientBuilder,
     RemoteDatabaseClient,
 ]
