a526f494eb84ffb638fc49a3d387885e0c80bbec
marianogaragiola <marianogaragiola@gmail.com>
2023-04-24 18:18:37 -0300

    feat: update remote database client for support File model

--- splight_lib/client/database/classmap.py
+++ splight_lib/client/database/classmap.py
@@ -4,4 +4,5 @@ from splight_lib.constants import ENGINE_PREFIX
 MODEL_NAME_MAP = {
     "asset": f"{ENGINE_PREFIX}/assets/",
     "attribute": f"{ENGINE_PREFIX}/attributes/",
+    "file": f"{ENGINE_PREFIX}/files/",
 }
