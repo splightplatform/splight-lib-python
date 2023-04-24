c18c41b6d5611af191a3caf2a1308c1f2157894e
marianogaragiola <marianogaragiola@gmail.com>
2023-04-24 18:18:16 -0300

    feat: add File model

--- splight_lib/models/__init__.py
+++ splight_lib/models/__init__.py
@@ -1,8 +1,10 @@
 from splight_lib.models.asset import Asset
 from splight_lib.models.attribute import Attribute
+from splight_lib.models.file import File
 
 
 __all__ = [
     Asset,
     Attribute,
+    File,
 ]
