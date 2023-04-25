042382a6b788f49f74fcfb0f3a79534c19a00841
marianogaragiola <marianogaragiola@gmail.com>
2023-04-25 14:39:01 -0300

    feat: add secret model

--- splight_lib/models/__init__.py
+++ splight_lib/models/__init__.py
@@ -2,10 +2,12 @@ from splight_lib.models.asset import Asset
 from splight_lib.models.attribute import Attribute
 from splight_lib.models.file import File
 from splight_lib.models.query import Query
+from splight_lib.models.secret import Secret
 
 __all__ = [
     Asset,
     Attribute,
     File,
     Query,
+    Secret,
 ]
