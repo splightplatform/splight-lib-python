0ae076de2508d078a47acf078bfb6c87837d23cb
marianogaragiola <marianogaragiola@gmail.com>
2023-04-25 15:16:12 -0300

    feat: add alert model

--- splight_lib/models/__init__.py
+++ splight_lib/models/__init__.py
@@ -1,3 +1,4 @@
+from splight_lib.models.alert import Alert, AlertCondition
 from splight_lib.models.asset import Asset
 from splight_lib.models.attribute import Attribute
 from splight_lib.models.file import File
@@ -5,6 +6,8 @@ from splight_lib.models.query import Query
 from splight_lib.models.secret import Secret
 
 __all__ = [
+    Alert,
+    AlertCondition,
     Asset,
     Attribute,
     File,
