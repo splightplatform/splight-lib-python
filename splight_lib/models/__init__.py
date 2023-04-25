fd53ecb00bddfa1ee73f49e7ec05779c718682b7
marianogaragiola <marianogaragiola@gmail.com>
2023-04-25 14:17:15 -0300

    feat: add query model

--- splight_lib/models/__init__.py
+++ splight_lib/models/__init__.py
@@ -1,10 +1,11 @@
 from splight_lib.models.asset import Asset
 from splight_lib.models.attribute import Attribute
 from splight_lib.models.file import File
-
+from splight_lib.models.query import Query
 
 __all__ = [
     Asset,
     Attribute,
     File,
+    Query,
 ]
