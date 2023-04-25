ed0e315574425f5da7cb0da38047a7d7e477c419
marianogaragiola <marianogaragiola@gmail.com>
2023-04-25 15:16:12 -0300

    feat: add alert model

---
+++ splight_lib/models/alert.py
@@ -0,0 +1,56 @@
+from enum import Enum
+from typing import Dict, List, Optional, TypedDict, Union
+
+from pydantic import BaseModel, EmailStr, Field
+
+from splight_lib.models.base import SplightDatabaseBaseModel
+
+# from uuid import UUID
+
+
+class AlertStatus(str, Enum):
+    ALERT = "alert"
+    NO_ALERT = "no_alert"
+    NO_DATA = "no_data"
+
+
+class AlertOperator(str, Enum):
+    GREATER_THAN = "gt"
+    GREATER_THAN_OR_EQUAL = "ge"
+    LOWER_THAN = "lt"
+    LOWER_THAN_OR_EQUAL = "le"
+    EQUAL = "eq"
+
+
+class AlertVariableType(str, Enum):
+    STR = "str"
+    INT = "int"
+    FLOAT = "float"
+
+
+class VariableDict(TypedDict):
+    name: str
+    type: str
+    value: Union[str, Dict]
+
+
+class AlertCondition(BaseModel):
+    id: Optional[str] = Field(None, max_length=100)
+    name: str
+    type: AlertVariableType = AlertVariableType.FLOAT
+    left_operand: str
+    right_operand: str
+    variables: List[VariableDict]
+    operator: AlertOperator = AlertOperator.EQUAL
+
+
+class Alert(SplightDatabaseBaseModel):
+    id: Optional[str] = Field(None, max_length=100)
+    name: str
+    descriptIon: Optional[str] = None
+    message: str
+    period: float = 1.0
+    active: bool = True
+    status: AlertStatus = AlertStatus.NO_ALERT
+    notification_emails: List[EmailStr] = Field(default_factory=list)
+    conditions: List[AlertCondition] = []
