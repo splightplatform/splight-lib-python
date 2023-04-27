e3a365de142f9b0462bad1e54476b08fcd70ebfe
marianogaragiola <marianogaragiola@gmail.com>
2023-04-27 17:13:04 -0300

    feat: add Component, ComponentObject and ComponentObjectInstance models

--- splight_lib/client/database/classmap.py
+++ splight_lib/client/database/classmap.py
@@ -5,6 +5,8 @@ MODEL_NAME_MAP = {
     "alert": f"{ENGINE_PREFIX}/alert/alerts/",
     "asset": f"{ENGINE_PREFIX}/assets/",
     "attribute": f"{ENGINE_PREFIX}/attributes/",
+    "component": f"{ENGINE_PREFIX}/component/components/",
+    "componentobject": f"{ENGINE_PREFIX}/component/objects/",
     "file": f"{ENGINE_PREFIX}/files/",
     "query": f"{ENGINE_PREFIX}/queries/",
     "secret": f"{ENGINE_PREFIX}/secrets/",
