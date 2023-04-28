93b41a72134617f14cbe1230e874c56ca064896d
marianogaragiola <marianogaragiola@gmail.com>
2023-04-28 09:07:03 -0300

    changes based on PR

--- splight_lib/client/exceptions.py
+++ splight_lib/client/exceptions.py
@@ -6,7 +6,7 @@ REQUEST_EXCEPTIONS = (ConnectionError, Timeout)
 SPLIGHT_REQUEST_EXCEPTIONS = (HTTPError, TimeoutError, ConnectError)
 
 
-class InvalidModel(Exception):
+class InvalidModelName(Exception):
     def __init__(self, model_name: str):
         self._msg = f"Model {model_name} is not a valid database model"
 
