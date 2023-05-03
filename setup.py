758408f1540e9c97b243a685a627f73f87ba8a9e
Agustin Curto <agustin.curto@splight-ai.com>
2023-05-03 11:29:32 -0300

    Testing github workflow

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ test_requires = [
 
 setup(
     name="splight-lib",
-    version="2.3.7",
+    version="2.3.8-dev0",
     author="Splight",
     author_email="factory@splight-ae.com",
     packages=find_packages(),
