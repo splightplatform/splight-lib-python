99021512c3eb2623cb13d33d3f6069a09915201d
Agustín Curto <agustincurtook@gmail.com>
2023-05-09 16:09:43 -0300

    Fix release workflow step

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ test_requires = [
 
 setup(
     name="splight-lib",
-    version="2.3.13",
+    version="2.3.14",
     author="Splight",
     author_email="factory@splight-ae.com",
     packages=find_packages(),
