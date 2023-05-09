f602c28499dea52ede18bdd36a3917c9c5234722
Agustin Curto <agustin.curto@splight-ai.com>
2023-05-09 16:08:10 -0300

    Update version to release

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ test_requires = [
 
 setup(
     name="splight-lib",
-    version="2.3.14.dev1",
+    version="2.3.14",
     author="Splight",
     author_email="factory@splight-ae.com",
     packages=find_packages(),
