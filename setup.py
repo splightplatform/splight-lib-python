a452e9734aea677bed80e0dc529fdc77e33c04b8
Agustin Curto <agustin.curto@splight-ai.com>
2023-05-09 12:20:08 -0300

    Clean up developer workflow

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ test_requires = [
 
 setup(
     name="splight-lib",
-    version="2.3.12.dev6",
+    version="2.3.12.dev7",
     author="Splight",
     author_email="factory@splight-ae.com",
     packages=find_packages(),
