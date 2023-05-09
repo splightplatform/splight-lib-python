7cbcf88f789f52533e876b590efca5d47f28ad08
Agustin Curto <agustin.curto@splight-ai.com>
2023-05-09 12:03:03 -0300

    test step

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ test_requires = [
 
 setup(
     name="splight-lib",
-    version="2.3.12.dev5",
+    version="2.3.12",
     author="Splight",
     author_email="factory@splight-ae.com",
     packages=find_packages(),
