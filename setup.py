c6fe793ea2fa61e00967c559c9befd8bc305ba18
Agustín Curto <agustincurtook@gmail.com>
2023-05-09 21:42:17 -0300

    Fix delete pypi version url (#282)
    Co-authored-by: Agustin Curto <agustin.curto@splight-ai.com>

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ test_requires = [
 
 setup(
     name="splight-lib",
-    version="2.3.14",
+    version="2.3.15",
     author="Splight",
     author_email="factory@splight-ae.com",
     packages=find_packages(),
