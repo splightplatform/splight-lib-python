fb9a318a25ab2c4fd5774290755c61b0466db1e0
Agustín Curto <agustincurtook@gmail.com>
2023-05-12 15:48:41 -0300

    Feature/move comm client (#283)
    * Move communication client from remote SL to SL
    
    * Move hub client from remote SL to SL
    
    * Remove remote splight lib
    
    * Create dev version
    
    * Remove hub local client
    
    * Update github action tag version
    
    * Remove remote splight lib for anywhere
    
    * Update github action tag version
    
    * Add debug workflow to test new action
    
    * Remove test
    
    ---------
    
    Co-authored-by: Agustin Curto <agustin.curto@splight-ai.com>

--- remote_splight_lib/auth/auth.py
+++
@@ -1,7 +0,0 @@
-class SplightAuthToken:
-    def __init__(self, access_key: str, secret_key: str):
-        self._token = f"Splight {access_key} {secret_key}"
-
-    @property
-    def header(self):
-        return {"Authorization": self._token}
--- remote_splight_lib/communication/__init__.py
+++
@@ -1,3 +0,0 @@
-from remote_splight_lib.communication.client import CommunicationClient
-
-__all__ = [CommunicationClient]
