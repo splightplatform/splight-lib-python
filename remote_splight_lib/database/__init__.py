f9c1f90da791f6b25de4b363580999e011028c32
Agustín Curto <agustincurtook@gmail.com>
2023-05-12 15:48:41 -0300

    Feature/move comm client (#283)
    * Move communication client from remote SL to SL
    
    * Move hub client from remote SL to SL
    
    * Remove remote splight lib
    
    * Create dev version
    
    * Remove hub local client
    
    * Update github action tag version
    
    * Remove remote splight lib for anywhere
    
    * Update github action tag version
    
    * Add debug workflow to test new action
    
    * Remove test
    
    ---------
    
    Co-authored-by: Agustin Curto <agustin.curto@splight-ai.com>

--- remote_splight_lib/database/__init__.py
+++
@@ -1,3 +0,0 @@
-from remote_splight_lib.database.client import DatabaseClient
-
-__all__ = [DatabaseClient]
---
+++ splight_lib/client/communication/local_client.py
@@ -0,0 +1,6 @@
+# TBD
+from splight_abstract.communication import AbstractCommunicationClient
+
+
+class LocalCommunicationClient(AbstractCommunicationClient):
+    pass
