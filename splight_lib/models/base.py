05f3f38b6d8646d33bf56e9796f3164fe2228ebc
marianogaragiola <marianogaragiola@gmail.com>
2023-05-17 14:30:48 -0300

    feat: method for creating indices in datalake

--- splight_lib/models/base.py
+++ splight_lib/models/base.py
@@ -1,6 +1,6 @@
 import json
 from datetime import datetime, timezone
-from typing import ClassVar, Dict, List, Optional
+from typing import ClassVar, Dict, List, Optional, Set, Tuple
 
 import pandas as pd
 from pydantic import BaseModel, Field, PrivateAttr
@@ -127,6 +127,25 @@ class SplightDatalakeBaseModel(BaseModel):
             k: v["id"] if isinstance(v, dict) and "id" in v.keys() else v
             for k, v in d.items()
         }
+
+    @classmethod
+    def create_indexes(cls, parameters: List[Dict]):
+        indices: Set[Tuple[str, int]] = set(
+            [
+                ("output_format", 1),
+                ("instance_id", 1),
+                ("instance_type", 1),
+                ("asset", 1),
+                ("attribute", 1),
+                ("timestamp", -1),
+            ]
+        )
+        for param in parameters:
+            if param["filterable"]:
+                indices.add((param["name"], 1))
+        indices = list(indices)
+        dl_client = cls.__get_datalake_client()
+        dl_client.create_index(cls._collection_name, indices)
 
     @staticmethod
     def __get_datalake_client() -> AbstractDatalakeClient:
