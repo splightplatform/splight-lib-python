536500816ec3fde499da5db13d528640222fbd43
marianogaragiola <marianogaragiola@gmail.com>
2023-05-19 18:26:55 -0300

    fix unit test

--- splight_lib/client/tests/test_database.py
+++ splight_lib/client/tests/test_database.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 
 class TestDatabase(TestCase):
     def test_import(self):
-        from splight_abstract.database import AbstractDatabaseClient
+        from splight_lib.client.database.abstract import AbstractDatabaseClient
 
 
 # TODO: complete tests for client/database/remote_client.py
