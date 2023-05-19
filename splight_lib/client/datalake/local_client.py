9834985bbc1ba8e829f271f337c937bc5128d3f0
marianogaragiola <marianogaragiola@gmail.com>
2023-05-19 13:15:19 -0300

    move abstracts for datalake client

--- splight_lib/client/datalake/local_client.py
+++ splight_lib/client/datalake/local_client.py
@@ -5,11 +5,11 @@ from functools import partial
 from typing import Dict, List, Optional, Union
 
 import pandas as pd
-from splight_abstract.datalake import AbstractDatalakeClient
+
+from splight_lib.client.datalake.abstract import AbstractDatalakeClient
 from splight_lib.client.file_handler import FixedLineNumberFileHandler
 from splight_lib.client.filter import value_filter
 from splight_lib.logging._internal import LogTags, get_splight_logger
-
 
 logger = get_splight_logger()
 
@@ -75,7 +75,7 @@ class LocalDatalakeClient(AbstractDatalakeClient):
             file_path=file_path, total_lines=self._TOTAL_DOCS
         )
         documents = [
-            json.loads(doc) for doc in handler.read()[skip_: skip_ + limit_]
+            json.loads(doc) for doc in handler.read()[skip_ : skip_ + limit_]
         ]
         documents = self._filter(documents, filters=filters)
 
