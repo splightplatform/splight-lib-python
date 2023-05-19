bd575649bc6138324aad3ebf6b6c684f7a171bd2
Agustin Curto <agustin.curto@splight-ai.com>
2023-05-19 10:41:54 -0300

    New tests structure

--- splight_lib/client/tests/test_datalake.py
+++ splight_lib/client/tests/test_datalake.py
@@ -4,3 +4,6 @@ from unittest import TestCase
 class TestDatalake(TestCase):
     def test_import_datalake(self):
         from splight_abstract.datalake import AbstractDatalakeClient
+
+
+# TODO: complete tests for client/datalake/remote_client.py
--- splight_lib/tests/test_notification.py
+++
@@ -1,6 +0,0 @@
-from unittest import TestCase
-
-
-class TestNotification(TestCase):
-    def test_import_notification(self):
-        from splight_abstract.notification import AbstractNotificationClient
