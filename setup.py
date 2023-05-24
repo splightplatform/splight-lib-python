e894a23476c3a6de0f5c752bd6189e52a59ecb40
smartinellimarco <smartinellimarco@icloud.com>
2023-05-24 16:34:33 -0300

    Bump version.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ test_requires = [
 
 setup(
     name="splight-lib",
-    version="2.3.20",
+    version="2.3.21",
     author="Splight",
     author_email="factory@splight-ae.com",
     packages=find_packages(),
