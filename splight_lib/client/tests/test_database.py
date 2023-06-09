6395f3ed4a0e6bc76495b57fbd64b0d30ad17259
lautaro <lautarolombardi19@gmail.com>
2023-06-09 11:39:47 -0300

    test draft

--- splight_lib/client/tests/test_database.py
+++ splight_lib/client/tests/test_database.py
@@ -1,9 +1,34 @@
 from unittest import TestCase
+from unittest.mock import patch
+
+from splight_lib.client.database.remote_client import RemoteDatabaseClient
 
 
-class TestDatabase(TestCase):
-    def test_import(self):
-        from splight_lib.client.database.abstract import AbstractDatabaseClient
+class TestRemoteDatabaseClient(TestCase):
+    def setUp(self):
+        self.patcher1 = patch(
+            "splight_lib.client.database.remote_client.SplightAuthToken"
+        )
+        self.patcher2 = patch(
+            "splight_lib.client.database.remote_client.SplightRestClient"
+        )
 
+        self.mock_auth_token = self.patcher1.start()
+        self.mock_rest_client = self.patcher2.start()
 
-# TODO: complete tests for client/database/remote_client.py
+        self.client = RemoteDatabaseClient(
+            base_url="http://test.com",
+            access_id="test_id",
+            secret_key="test_secret",
+        )
+
+    def tearDown(self):
+        self.patcher1.stop()
+        self.patcher2.stop()
+
+    def test_initialization(self):
+        self.mock_auth_token.assert_called_once_with(
+            access_key="test_id", secret_key="test_secret"
+        )
+        assert self.client._base_url.url == "http://test.com"
+        self.mock_rest_client().update_headers.assert_called_once()
