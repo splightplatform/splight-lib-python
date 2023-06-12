3bdfea078565222a5c595aa6218f7182917f0d78
lautaro <lautarolombardi19@gmail.com>
2023-06-12 16:51:07 -0300

    patch http methods in datalake

--- splight_lib/client/tests/test_datalake.py
+++ splight_lib/client/tests/test_datalake.py
@@ -1,12 +1,28 @@
 import os
-from unittest.mock import patch  # noqa E402
+from unittest.mock import Mock, patch  # noqa E402
 
 import pandas as pd  # noqa E402
 from splight_lib.client.datalake import RemoteDatalakeClient  # noqa F402
+from splight_lib.client.datalake.remote_client import (  # noqa F402
+    SplightRestClient,
+)
 
 base_url = "http://test.com"
-access_id = os.environ["SPLIGHT_ACCESS_ID"]
-secret_key = os.environ["SPLIGHT_SECRET_KEY"]
+access_id = "access_id"
+secret_key = "secret_key"
+
+
+class MockResponse:
+    status_code = 200
+
+    def __init__(self, json_data):
+        self.json_data = json_data
+
+    def raise_for_status(self):
+        return None
+
+    def json(self):
+        return self.json_data
 
 
 @patch("splight_lib.client.datalake.remote_client.SplightAuthToken")
@@ -24,11 +40,10 @@ def test_initialization(mock_rest_client, mock_auth_token):
     mock_rest_client.assert_called_once()
 
 
-@patch("splight_lib.client.datalake.remote_client.SplightRestClient")
-def test_save(mock_rest_client):
-    mock_rest_client.return_value.post.return_value.raise_for_status.return_value = (
-        None
-    )
+@patch.object(
+    SplightRestClient, "post", return_value=MockResponse(({"key": "value"}))
+)
+def test_save(mock_post):
     client = RemoteDatalakeClient(
         base_url=base_url,
         access_id=access_id,
@@ -37,73 +52,56 @@ def test_save(mock_rest_client):
     collection = "collection_name"
     instances = [{"key": "value"}]
     result = client.save(collection=collection, instances=instances)
-    mock_rest_client.return_value.post.assert_called_once()
+    mock_post.assert_called_once()
     assert result == instances
 
 
-@patch("splight_lib.client.datalake.remote_client.SplightRestClient")
-def test_raw_get(mock_rest_client):
-    response_data = {"results": [{"key": "value"}]}
-    mock_rest_client.return_value.get.return_value.json.return_value = (
-        response_data
-    )
-    mock_rest_client.return_value.get.return_value.raise_for_status.return_value = (
-        None
-    )
+@patch.object(
+    SplightRestClient,
+    "get",
+    return_value=MockResponse({"results": [{"key": "value"}]}),
+)
+def test_raw_get(mock_get):
     client = RemoteDatalakeClient(base_url, access_id, secret_key)
     collection = "collection_name"
     result = client._raw_get("resource", collection)
-    mock_rest_client.return_value.get.assert_called_once()
-    assert result == response_data["results"]
+    mock_get.assert_called_once()
+    assert result == [{"key": "value"}]
 
 
-@patch("splight_lib.client.datalake.remote_client.SplightRestClient")
-def test_delete(mock_rest_client):
-    mock_rest_client.return_value.delete.return_value.raise_for_status.return_value = (
-        None
-    )
+@patch.object(SplightRestClient, "delete", return_value=MockResponse(None))
+def test_delete(mock_delete):
     client = RemoteDatalakeClient(base_url, access_id, secret_key)
     collection = "collection_name"
     client.delete(collection)
-    mock_rest_client.return_value.delete.assert_called_once()
+    mock_delete.assert_called_once()
 
 
-@patch("splight_lib.client.datalake.remote_client.SplightRestClient")
-def test_save_dataframe(mock_rest_client):
-    mock_rest_client.return_value.post.return_value.raise_for_status.return_value = (
-        None
-    )
+@patch.object(SplightRestClient, "post", return_value=MockResponse(None))
+def test_save_dataframe(mock_post):
     client = RemoteDatalakeClient(base_url, access_id, secret_key)
     collection = "collection_name"
     dataframe = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
     client.save_dataframe(collection, dataframe)
-    mock_rest_client.return_value.post.assert_called_once()
+    mock_post.assert_called_once()
 
 
-@patch("splight_lib.client.datalake.remote_client.SplightRestClient")
-def test_create_index(mock_rest_client):
-    mock_rest_client.return_value.post.return_value.raise_for_status.return_value = (
-        None
-    )
+@patch.object(SplightRestClient, "post", return_value=MockResponse(None))
+def test_create_index(mock_post):
     client = RemoteDatalakeClient(base_url, access_id, secret_key)
     collection = "collection_name"
     indexes = [{"key": 1}]
     client.create_index(collection, indexes)
-    mock_rest_client.return_value.post.assert_called_once()
+    mock_post.assert_called_once()
 
 
-@patch("splight_lib.client.datalake.remote_client.SplightRestClient")
-def test_raw_aggregate(mock_rest_client):
-    response_data = [{"key": "value"}]
-    mock_rest_client.return_value.post.return_value.json.return_value = (
-        response_data
-    )
-    mock_rest_client.return_value.post.return_value.raise_for_status.return_value = (
-        None
-    )
+@patch.object(
+    SplightRestClient, "post", return_value=MockResponse([{"key": "value"}])
+)
+def test_raw_aggregate(mock_post):
     client = RemoteDatalakeClient(base_url, access_id, secret_key)
     collection = "collection_name"
     pipeline = [{"$match": {"key": "value"}}]
     result = client.raw_aggregate(collection, pipeline)
-    mock_rest_client.return_value.post.assert_called_once()
-    assert result == response_data
+    mock_post.assert_called_once()
+    assert result == [{"key": "value"}]
