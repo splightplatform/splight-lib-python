975cd7bf260e6006ed81152a386e96b38a2220b0
marianogaragiola <marianogaragiola@gmail.com>
2023-07-06 11:22:34 -0300

    delete commented code

--- splight_lib/logging/logging.py
+++ splight_lib/logging/logging.py
@@ -30,7 +30,6 @@ class SplightFormatter(Formatter):
                 fmt = " | ".join([fmt, "%(tags)s"])
         except AttributeError:
             pass  # tags aren't present
-        # formatter = Formatter(fmt=fmt, datefmt="%Y-%m-%dT%H:%M:%S%z")
         formatter = Formatter(fmt=fmt, datefmt="%Y-%m-%dT%H:%M:%SZ")
         formatter.converter = time.gmtime
         return formatter.format(record)
