09bc7ff3855cf9e864ab6306c4bb5859102e3979
marianogaragiola <marianogaragiola@gmail.com>
2023-07-25 20:33:06 -0300

    small changes for making log streamer more robust

--- splight_lib/component/log_streamer.py
+++ splight_lib/component/log_streamer.py
@@ -1,9 +1,9 @@
 import re
 import sys
+from queue import Empty, Queue
 from subprocess import Popen
-from typing import Generator, Optional
-from queue import Queue, Empty
 from threading import Thread
+from typing import Generator, Optional
 
 from splight_lib.client.grpc.client import LogsGRPCClient
 from splight_lib.settings import settings
@@ -25,9 +25,9 @@ class ComponentLogsStreamer:
             secret_key=settings.SPLIGHT_SECRET_KEY,
         )
         self._logs_entry = self._client._log_entry
-        self._thread = None
-        self._queue = None
-        self._running = False
+        self._thread: Optional[Thread] = None
+        self._queue: Optional[Queue] = None
+        self._running: bool = False
 
     def start(self):
         self._thread = Thread(target=self._consume_logs, daemon=True)
@@ -37,88 +37,51 @@ class ComponentLogsStreamer:
         self._run()
 
     def stop(self):
-        # self._process.kill()
         self._running = False
         self._thread.join(timeout=10)
         self._queue = None
         self._thread = None
 
     def _run(self):
-        # for msg in self.logs_iterator():
-        #     msg = msg.rstrip()
-        #     print(msg)
-        while True:
+        while self._running:
             try:
-                self._client.stream_logs(self.logs_iterator, self._component_id)
+                self._client.stream_logs(
+                    self.logs_iterator, self._component_id
+                )
             except Exception:
-                print("Stream Stopped")
+                print("Log Streamer stopped")
                 break
 
     def _consume_logs(self):
         reader = iter(self._process.stdout.readline, "")
         for new_line in reader:
             if self._process.poll() is not None:
-                # msg = "".join(self._message_buffer)
-                # sys.stdout.write(msg)
-                # yield msg
                 output, error = self._process.communicate()
                 output_msg = output.decode("utf-8")
                 error_msg = error.decode("utf-8")
-                # sys.stdout.write(output_msg)
                 self._queue.put(output_msg)
-                # sys.stdout.write(error_msg)
                 self._queue.put(error_msg)
+                self._running = False
                 break
             self._queue.put(new_line.decode("utf-8"))
 
-    # def _restart(self):
-    #     print("Restarting")
-    #     self.stop()
-    #     self.start()
-
     def logs_iterator(self) -> Generator:
         self._message_buffer = []
-        while self._running:
+        while True:
             try:
                 message = self._queue.get(timeout=10)
-                # message = self._queue.get()
             except Empty:
                 msg = "".join(self._message_buffer)
                 if msg:
                     sys.stdout.write(msg)
                     yield msg
-                # self._restart()
                 return
-                # break
 
-            # yield message
             full_msg = self._generate_message(message)
             if not full_msg:
                 continue
             yield full_msg
             sys.stdout.write(full_msg)
-        # reader = iter(self._process.stdout.readline, "")
-        # for new_line in reader:
-        #     if self._process.poll() is not None:
-        #         msg = "".join(self._message_buffer)
-        #         sys.stdout.write(msg)
-        #         yield msg
-        #         output, error = self._process.communicate()
-        #         output_msg = output.decode("utf-8")
-        #         error_msg = error.decode("utf-8")
-        #         sys.stdout.write(output_msg)
-        #         yield output_msg
-        #         sys.stdout.write(error_msg)
-        #         yield error_msg
-        #         break
-        #
-        #     line_msg = new_line.decode("utf-8")
-        #     sys.stdout.write(line_msg)
-        #
-        #     full_msg = self._generate_message(line_msg)
-        #     if not full_msg:
-        #         continue
-        #     yield full_msg
 
     def _generate_message(self, raw_msg: str) -> Optional[str]:
         if self._is_log(raw_msg):
