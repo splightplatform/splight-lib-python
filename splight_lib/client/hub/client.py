a43347f0bf73a4cfbe77901bf509b3e42fa272df
smartinellimarco <smartinellimarco@gmail.com>
2023-08-25 12:00:21 -0300

    Fix pre-commit config for black and isort. Use pyproject.toml

--- splight_lib/client/hub/client.py
+++ splight_lib/client/hub/client.py
@@ -3,6 +3,7 @@ from typing import Dict, List, Tuple
 import requests
 from furl import furl
 from pydantic import BaseModel
+
 from splight_lib.auth import SplightAuthToken
 from splight_lib.client.hub.abstract import AbstractHubClient
 
