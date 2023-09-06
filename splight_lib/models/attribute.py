29dc20bbd7290d509f10128468386d19a53e5ea9
marianogaragiola <marianogaragiola@gmail.com>
2023-09-06 11:30:16 -0300

    fix: update Attribute model for sync with API

--- splight_lib/models/attribute.py
+++ splight_lib/models/attribute.py
@@ -1,9 +1,22 @@
 from typing import Optional
+from enum import auto
+
+from strenum import PascalCaseStrEnum
 
 from splight_lib.models.base import SplightDatabaseBaseModel
+
+
+class AttributeType(PascalCaseStrEnum):
+    NUMBER = auto()
+    BOOLEAN = auto()
+    STRING = auto()
 
 
 class Attribute(SplightDatabaseBaseModel):
     id: Optional[str]
     name: str
     asset: str
+    type: AttributeType = AttributeType.NUMBER
+
+    def save(self):
+        raise NotImplementedError("Attribute object can't be saved")
