7b261b104a909c3531d457c708c4e40f54cb84a8
marianogaragiola <marianogaragiola@gmail.com>
2023-09-14 11:49:29 -0300

    update version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ test_requires = [
 
 setup(
     name="splight-lib",
-    version="3.3.5",
+    version="3.3.6",
     author="Splight",
     author_email="factory@splight-ae.com",
     packages=find_packages(),
