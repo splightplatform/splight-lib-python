9347de1b20851f63e58953e0a99bbaf5a1b04439
marianogaragiola <marianogaragiola@gmail.com>
2023-10-06 09:52:47 -0300

    minor changes

--- splight_lib/__init__.py
+++ splight_lib/__init__.py
@@ -1,1 +1,3 @@
 from splight_lib.version import __version__
+
+__all__ = [__version__]
--- splight_lib/version.py
+++ splight_lib/version.py
@@ -1,4 +1,3 @@
 from importlib import metadata
 
 __version__ = metadata.version("splight_lib")
-# __version__ = "3.3.8"
