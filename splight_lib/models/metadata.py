c67bcee2e52e6a1b51f2519d044aa92c7ae83e16
facumartin <facundo.martin.9@gmail.com>
2023-10-17 14:57:24 -0300

    add metadata to assets

---
+++ splight_lib/models/metadata.py
@@ -0,0 +1,19 @@
+from enum import auto
+from typing import Optional
+
+from strenum import PascalCaseStrEnum
+
+from splight_lib.models.base import SplightDatabaseBaseModel
+
+
+class MetadataType(PascalCaseStrEnum):
+    NUMBER = auto()
+    BOOLEAN = auto()
+    STRING = auto()
+
+
+class Metadata(SplightDatabaseBaseModel):
+    id: Optional[str]
+    name: Optional[str]
+    asset: Optional[str]
+    type: MetadataType = MetadataType.STRING
