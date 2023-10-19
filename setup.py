e7dffa14b8dc03cc82375aad46aef77a3dd076be
facumartin <facundo.martin.9@gmail.com>
2023-10-19 11:02:08 -0300

    version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ test_requires = [
 
 setup(
     name="splight-lib",
-    version="3.3.8",
+    version="3.3.11",
     author="Splight",
     author_email="factory@splight-ae.com",
     packages=find_packages(),
