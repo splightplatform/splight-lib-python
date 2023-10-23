5816a92a54f7de5bd547f99015c6a605ab98da7f
franco <francozacco@gmail.com>
2023-10-23 10:46:49 +0200

    updates action

---
+++ .github/workflows/scripts/check_dev_version.py
@@ -0,0 +1,28 @@
+import sys
+
+from pkg_resources import parse_version as parse
+
+
+class VersionError(Exception):
+    """Raised when the version is not a developer version."""
+
+    ...
+
+
+if __name__ == "__main__":
+    """Checks if the version is a developer version.
+
+    Parameters
+    ----------
+    version: The version from the feature branch
+
+    Raises
+    ------
+    VersionError if the version is not a developer version
+    """
+    version_line = sys.argv[1]
+
+    version = parse(version_line)
+
+    if not version.is_devrelease:
+        raise VersionError(f"Version {version} is not a developer version")
