48e7b6014e79515534b533b4d513e014f22a46a3
Lautaro Lecumberry <lautarolecumberry@users.noreply.github.com>
2023-11-14 11:48:35 -0300

    create dev version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ test_requires = [
 
 setup(
     name="splight-lib",
-    version="4.3.0",
+    version="4.3.0.dev0",
     author="Splight",
     author_email="factory@splight-ae.com",
     packages=find_packages(),
