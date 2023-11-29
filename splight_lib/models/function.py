f8cd8a137c501a3fa5a85ee79aa367c28b4cf34a
franco <francozacco@gmail.com>
2023-11-29 12:53:20 +0100

    adding support for plain expressions and plain queries

--- splight_lib/models/function.py
+++ splight_lib/models/function.py
@@ -1,7 +1,15 @@
+import json
+import re
 from enum import auto
 from typing import List, Literal, Optional
 
-from pydantic import BaseModel, Field, PositiveInt
+from pydantic import (
+    BaseModel,
+    Field,
+    PositiveInt,
+    ValidationError,
+    model_validator,
+)
 from strenum import LowercaseStrEnum, UppercaseStrEnum
 from typing_extensions import TypedDict
 
@@ -49,6 +57,55 @@ class FunctionItem(BaseModel):
     query_sort_field: Optional[str] = None
     query_sort_direction: Optional[int] = None
     query_plain: Optional[str] = None
+
+    @model_validator(mode="after")
+    def validate_expression(self):
+        if self.expression:
+            if self.type != FunctionItemType.EXPRESSION:
+                raise ValidationError(
+                    "type must be EXPRESSION if a expression is passed."
+                )
+            self.expression_plain = (
+                self._get_expression_plain()
+                if self.expression_plain is None
+                else self.expression_plain
+            )
+        return self
+
+    @model_validator(mode="after")
+    def validate_query(self):
+        if self.query_filter_asset is not None:
+            if self.type != FunctionItemType.QUERY:
+                raise ValidationError(
+                    "type must be QUERY if a query_filter_asset is passed."
+                )
+            self.query_plain = (
+                self._get_query_plain()
+                if self.query_plain is None
+                else self.query_plain
+            )
+        return self
+
+    def _get_expression_plain(self):
+        pattern = r"\$\w+"
+        args = re.findall(pattern, self.expression)
+        str_args = ", ".join(args)
+        body = f"function ({str_args}) {{ return {self.expression} }}"
+        expression_plain = {
+            "$function": {"body": body, "args": args, "lang": "js"}
+        }
+        return json.dumps(expression_plain)
+
+    def _get_query_plain(self):
+        query_plain = [
+            {
+                "$match": {
+                    "asset": self.query_filter_asset["id"],
+                    "attribute": self.query_filter_attribute["id"],
+                }
+            }
+        ]
+        return json.dumps(query_plain)
 
 
 class Function(SplightDatabaseBaseModel):
