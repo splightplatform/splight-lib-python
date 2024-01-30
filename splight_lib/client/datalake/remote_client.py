d77e276cb5ba9b399453b9d4988a77342aba1e16
marianogaragiola <marianogaragiola@gmail.com>
2024-01-30 12:46:27 -0300

    feat: remote datalake client with save and get dataframe

--- splight_lib/client/datalake/remote_client.py
+++ splight_lib/client/datalake/remote_client.py
@@ -1,9 +1,6 @@
-from datetime import datetime, timedelta, timezone
-from io import StringIO
-from tempfile import NamedTemporaryFile
 from threading import Lock, Thread
 from time import sleep
-from typing import Dict, List, Optional, Union
+from typing import Dict, List, Union
 
 import pandas as pd
 from furl import furl
@@ -15,7 +12,12 @@ from splight_lib.client.datalake.buffer import DatalakeDocumentBuffer
 from splight_lib.client.datalake.exceptions import InvalidCollectionName
 from splight_lib.client.datalake.schemas import DataRequest
 from splight_lib.client.exceptions import SPLIGHT_REQUEST_EXCEPTIONS
-from splight_lib.constants import DL_BUFFER_SIZE, DL_BUFFER_TIMEOUT
+from splight_lib.constants import (
+    DEFAULT_COLLECTION,
+    DEFAULT_SORT_FIELD,
+    DL_BUFFER_SIZE,
+    DL_BUFFER_TIMEOUT,
+)
 from splight_lib.logging._internal import LogTags, get_splight_logger
 from splight_lib.restclient import SplightRestClient
 from splight_lib.stringcase import camelcase
@@ -47,6 +49,7 @@ class RemoteDatalakeClient(AbstractDatalakeClient):
         collection: str,
         instances: Union[List[Dict], Dict],
     ) -> List[dict]:
+        # POST /data/write
         instances = instances if isinstance(instances, list) else [instances]
         url = self._base_url / f"{self._PREFIX}/write"
         collection = camelcase(collection)
@@ -64,6 +67,7 @@ class RemoteDatalakeClient(AbstractDatalakeClient):
         collection: str,
         instances: Union[List[Dict], Dict],
     ) -> List[dict]:
+        # POST /data/write
         instances = instances if isinstance(instances, list) else [instances]
         url = self._base_url / f"{self._PREFIX}/write"
         data = {
@@ -77,14 +81,14 @@ class RemoteDatalakeClient(AbstractDatalakeClient):
     @retry(SPLIGHT_REQUEST_EXCEPTIONS, tries=3, delay=2, jitter=1)
     def _get(
         self,
-        asset: str,
-        attribute: str,
-        collection: str = "default",
-        sort_field: str = "timestamp",
+        match: Dict[str, str],
+        collection: str = DEFAULT_COLLECTION,
+        sort_field: str = DEFAULT_SORT_FIELD,
         sort_direction: int = -1,
         limit: int = 10000,
         **filters,
-    ):
+    ) -> List[Dict]:
+        # POST /data/read
         url = self._base_url / f"{self._PREFIX}/read"
         data_request = DataRequest(
             collection=collection,
@@ -95,14 +99,7 @@ class RemoteDatalakeClient(AbstractDatalakeClient):
                 {
                     "ref_id": "output",
                     "type": "QUERY",
-                    "pipeline": [
-                        {
-                            "$match": {
-                                "asset": asset,
-                                "attribute": attribute,
-                            }
-                        }
-                    ],
+                    "pipeline": [{"$match": match}],
                     "expression": None,
                 }
             ],
@@ -113,30 +110,16 @@ class RemoteDatalakeClient(AbstractDatalakeClient):
         return response.json()
 
     @retry(SPLIGHT_REQUEST_EXCEPTIONS, tries=3, delay=2, jitter=1)
-    def raw_get(
-        self,
-        data_request: DataRequest,
-    ) -> List[Dict]:
-        url = self._base_url / f"{self._PREFIX}/read"
-
-        response = self._restclient.post(url, json=data_request.dict())
-        response.raise_for_status()
-        output = response.json()
-
-        return output
-
-    @retry(SPLIGHT_REQUEST_EXCEPTIONS, tries=3, delay=2, jitter=1)
     async def _async_get(
         self,
-        asset: str,
-        attribute: str,
-        collection: str = "default",
-        sort_field: str = "timestamp",
+        match: Dict[str, str],
+        collection: str = DEFAULT_COLLECTION,
+        sort_field: str = DEFAULT_SORT_FIELD,
         sort_direction: int = -1,
         limit: int = 10000,
         **filters,
     ) -> List[Dict]:
-        # GET /datalake/data/
+        # GET /data/read
         url = self._base_url / f"{self._PREFIX}/read"
         data_request = DataRequest(
             collection=collection,
@@ -149,10 +132,7 @@ class RemoteDatalakeClient(AbstractDatalakeClient):
                     "type": "QUERY",
                     "pipeline": [
                         {
-                            "$match": {
-                                "asset": asset,
-                                "attribute": attribute,
-                            }
+                            "$match": match,
                         }
                     ],
                     "expression": None,
@@ -164,46 +144,34 @@ class RemoteDatalakeClient(AbstractDatalakeClient):
         response.raise_for_status()
         return response.json()
 
-    @retry(SPLIGHT_REQUEST_EXCEPTIONS, tries=3, delay=2, jitter=1)
-    def delete(self, collection: str, **kwargs) -> None:
-        # DELETE /datalake/delete/
-        url = self._base_url / f"{self._PREFIX}/delete/"
-        params = {"source": collection}
-        response = self._restclient.delete(url, params=params, json=kwargs)
-        response.raise_for_status()
+    def save_dataframe(
+        self, dataframe: pd.DataFrame, collection: str = DEFAULT_COLLECTION
+    ) -> None:
+        dataframe["timestamp"] = pd.Series(
+            dataframe["timestamp"].dt.to_pydatetime(),
+            dtype="object",
+        ).apply(lambda x: x.isoformat())
+        docs = dataframe.to_dict("records")
+        self.save(collection, docs)
 
-    @retry(SPLIGHT_REQUEST_EXCEPTIONS, tries=3, delay=2, jitter=1)
-    def execute_query(
+    def get_dataframe(
         self,
-        from_timestamp: datetime,
-        to_timestamp: Optional[datetime],
-        query: Dict,
+        match: Dict[str, str],
+        collection: str = DEFAULT_COLLECTION,
+        sort_field: str = DEFAULT_SORT_FIELD,
+        sort_direction: int = -1,
+        limit: int = 10000,
+        **filters,
     ) -> pd.DataFrame:
-        url = self._base_url / f"{self._PREFIX}/data/request/csv/"
-        to_timestamp = to_timestamp.isoformat() if to_timestamp else None
-        data = {
-            "from_timestamp": from_timestamp.isoformat(),
-            "to_timestamp": to_timestamp,
-            "traces": [
-                {
-                    "ref_id": "output",
-                    "type": "QUERY",
-                    "expression": None,
-                    "pipeline": query,
-                }
-            ],
-        }
-        response = self._restclient.post(url, json=data)
-        response.raise_for_status()
-
-        df: pd.DataFrame = pd.DataFrame(
-            pd.read_csv(StringIO(response.text), index_col=False)
+        data = self._get(
+            match, collection, sort_field, sort_direction, limit, **filters
         )
-        if df.empty:
-            return df
-        df["timestamp"] = pd.to_datetime(df["timestamp"])
-        df.drop(labels="Unnamed: 0", axis=1, inplace=True)
-        df.set_index("timestamp", inplace=True)
+        df = pd.DataFrame(data)
+        df.index = pd.to_datetime(df["timestamp"])
+        df.drop(columns=["timestamp"], inplace=True)
+        df.rename(columns={"output": "value"}, inplace=True)
+        for key, value in match.items():
+            df[key] = value
         return df
 
 
@@ -262,8 +230,14 @@ class BufferedRemoteDatalakeClient(RemoteDatalakeClient):
             self._data_buffers[collection].add_documents(instances)
         return instances
 
-    def save_dataframe(self, collection: str, dataframe: pd.DataFrame) -> None:
+    def save_dataframe(
+        self, dataframe: pd.DataFrame, collection: str = DEFAULT_COLLECTION
+    ) -> None:
         logger.debug("Saving dataframe in datalake")
+        dataframe["timestamp"] = pd.Series(
+            dataframe["timestamp"].dt.to_pydatetime(),
+            dtype="object",
+        ).apply(lambda x: x.isoformat())
         instances = dataframe.to_dict("records")
         self.save(collection, instances)
 
@@ -285,10 +259,13 @@ class BufferedRemoteDatalakeClient(RemoteDatalakeClient):
 
     @retry(SPLIGHT_REQUEST_EXCEPTIONS, tries=3, delay=2, jitter=1)
     def _send_documents(self, collection: str, docs: List[Dict]) -> List[Dict]:
-        url = self._base_url / f"{self._PREFIX}/save/"
+        url = self._base_url / f"{self._PREFIX}/write"
         collection = camelcase(collection)
-        response = self._restclient.post(
-            url, params={"source": collection}, json=docs
-        )
+        collection = camelcase(collection)
+        data = {
+            "collection": collection,
+            "records": docs,
+        }
+        response = self._restclient.post(url, json=data)
         response.raise_for_status()
         return docs
