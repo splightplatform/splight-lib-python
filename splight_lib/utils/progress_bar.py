0f0f0f537d5a0bc40ee841c431046338a0413ac9
marianogaragiola <marianogaragiola@gmail.com>
2024-03-05 10:02:49 -0300

    feat: add function with the progress bar for download files

---
+++ splight_lib/utils/progress_bar.py
@@ -0,0 +1,11 @@
+import sys
+
+
+def bar_progress(current: float, total: float, width: int = 80):
+    progress_message = "Downloading: %d%% [%d / %d] bytes" % (
+        current / total * 100,
+        current,
+        total,
+    )
+    sys.stdout.write("\r" + progress_message)
+    sys.stdout.flush()
