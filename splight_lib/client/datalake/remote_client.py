190f1e46823a77567dcefa5e967eef9c4f93db69
smartinellimarco <smartinellimarco@gmail.com>
2024-03-25 19:45:59 -0300

    Fix typo

--- splight_lib/client/datalake/remote_client.py
+++ splight_lib/client/datalake/remote_client.py
@@ -133,7 +133,7 @@ class SyncRemoteDatalakeClient(AbstractDatalakeClient):
         url = self._base_url / f"{self._PREFIX}/read"
         pipeline = [{"$match": match}, *extra_pipeline]
         data_request = DataRequest(
-            from_timestmap=from_timestamp,
+            from_timestamp=from_timestamp,
             to_timestamp=to_timestamp,
             collection=collection,
             sort_field=sort_field,
