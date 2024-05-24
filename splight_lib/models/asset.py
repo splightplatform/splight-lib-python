616b35faba8ab84e300cb58ad900a2389e966c12
marianogaragiola <marianogaragiola@gmail.com>
2024-05-24 09:10:03 -0300

    feat: support for related asset with related_to and related_from

--- splight_lib/models/asset.py
+++ splight_lib/models/asset.py
@@ -2,7 +2,7 @@ import warnings
 from typing import Any, List, Optional, Tuple
 
 from geojson_pydantic import GeometryCollection
-from pydantic import Field
+from pydantic import BaseModel, Field
 
 from splight_lib.constants import DESCRIPTION_MAX_LENGTH
 from splight_lib.models.attribute import Attribute
@@ -10,6 +10,20 @@ from splight_lib.models.base import SplightDatabaseBaseModel
 from splight_lib.models.metadata import Metadata
 
 warnings.filterwarnings("ignore", category=UserWarning)
+
+
+class AssetRepr(BaseModel):
+    id: str
+    name: str
+
+
+class AssetRelationship(BaseModel):
+    id: str
+    name: str
+    description: Optional[str] = None
+    related_asset_kind: Optional[str] = None
+    asset: AssetRepr
+    related_asset: AssetRepr
 
 
 class Asset(SplightDatabaseBaseModel):
@@ -23,7 +37,8 @@ class Asset(SplightDatabaseBaseModel):
     metadata: List[Metadata] = []
     geometry: Optional[GeometryCollection] = None
     centroid_coordinates: Optional[Tuple[float, float]] = None
-    related_assets: Optional[List["Asset"]] = []
+    related_to: List[AssetRelationship] = []
+    related_from: List[AssetRelationship] = []
 
     def set_attribute(self, attribute: Attribute, value: Any, value_type: str):
         new_value = self._db_client.operate(
