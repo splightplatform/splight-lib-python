d09cb49074fedde57a3e3cc0828e007866e75dff
marianogaragiola <marianogaragiola@gmail.com>
2024-08-31 17:17:13 -0300

    feat: update datalake client

--- splight_lib/client/datalake/remote_client.py
+++ splight_lib/client/datalake/remote_client.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from threading import Lock, Thread
 from time import sleep
-from typing import Dict, List, Optional, Union
+from typing import Any, TypedDict
 
 import pandas as pd
 from furl import furl
@@ -10,10 +10,7 @@ from retry import retry
 from splight_lib.auth import SplightAuthToken
 from splight_lib.client.datalake.abstract import AbstractDatalakeClient
 from splight_lib.client.datalake.buffer import DatalakeDocumentBuffer
-from splight_lib.client.datalake.exceptions import (
-    DatalakeRequestError,
-    InvalidCollectionName,
-)
+from splight_lib.client.datalake.exceptions import DatalakeRequestError
 from splight_lib.client.exceptions import SPLIGHT_REQUEST_EXCEPTIONS
 from splight_lib.constants import (
     DEFAULT_COLLECTION,
@@ -23,9 +20,13 @@ from splight_lib.constants import (
 )
 from splight_lib.logging._internal import LogTags, get_splight_logger
 from splight_lib.restclient import SplightRestClient
-from splight_lib.stringcase import camelcase
 
 logger = get_splight_logger()
+
+
+class Records(TypedDict):
+    collection: str
+    records: list[dict[str, Any]]
 
 
 class SyncRemoteDatalakeClient(AbstractDatalakeClient):
@@ -47,39 +48,24 @@ class SyncRemoteDatalakeClient(AbstractDatalakeClient):
         )
 
     @retry(SPLIGHT_REQUEST_EXCEPTIONS, tries=3, delay=2, jitter=1)
-    def save(
-        self,
-        collection: str,
-        instances: Union[List[Dict], Dict],
-    ) -> List[dict]:
-        # POST /data/write
-        instances = instances if isinstance(instances, list) else [instances]
+    def save(self, records: Records) -> list[dict]:
         url = self._base_url / f"{self._PREFIX}/write"
-        collection = camelcase(collection)
-        data = {
-            "collection": collection,
-            "records": instances,
-        }
-        response = self._restclient.post(url, json=data)
-        response.raise_for_status()
-        return instances
+        response = self._restclient.post(url, json=records)
+        if response.is_error:
+            raise DatalakeRequestError(response.status_code, response.text)
+        return records["records"]
 
     @retry(SPLIGHT_REQUEST_EXCEPTIONS, tries=3, delay=2, jitter=1)
     async def async_save(
         self,
-        collection: str,
-        instances: Union[List[Dict], Dict],
-    ) -> List[dict]:
+        records: Records,
+    ) -> list[dict]:
         # POST /data/write
-        instances = instances if isinstance(instances, list) else [instances]
         url = self._base_url / f"{self._PREFIX}/write"
-        data = {
-            "collection": collection,
-            "records": instances,
-        }
-        response = await self._restclient.async_post(url, json=data)
-        response.raise_for_status()
-        return instances
+        response = await self._restclient.async_post(url, json=records)
+        if response.is_error:
+            raise DatalakeRequestError(response.status_code, response.text)
+        return records["records"]
 
     @retry(SPLIGHT_REQUEST_EXCEPTIONS, tries=3, delay=2, jitter=1)
     def _get(self, request: dict) -> list[dict]:
@@ -100,24 +86,22 @@ class SyncRemoteDatalakeClient(AbstractDatalakeClient):
     def save_dataframe(
         self, dataframe: pd.DataFrame, collection: str = DEFAULT_COLLECTION
     ) -> None:
-        dataframe["timestamp"] = dataframe["timestamp"].apply(
-            lambda x: x.tz_localize(tz="UTC").isoformat()
-        )
-        docs = dataframe.to_dict("records")
-        self.save(collection, docs)
+        dataframe = _fix_dataframe_timestamp(dataframe)
+        instances = dataframe.to_dict("records")
+        self.save({"collection": collection, "records": instances})
 
     def get_dataframe(
         self,
-        match: Dict[str, str],
+        match: dict[str, str],
         collection: str = DEFAULT_COLLECTION,
         sort_field: str = DEFAULT_SORT_FIELD,
         sort_direction: int = -1,
-        from_timestamp: Optional[datetime] = None,
-        to_timestamp: Optional[datetime] = None,
-        extra_pipeline: List[Dict] = [],
-        aggregation_query: Dict = {},
+        from_timestamp: datetime | None = None,
+        to_timestamp: datetime | None = None,
+        extra_pipeline: list[dict] = [],
+        aggregation_query: dict = {},
         limit: int = 10000,
-        max_time_ms: Optional[int] = 10000,
+        max_time_ms: int = 10000,
         **kwargs,
     ) -> pd.DataFrame:
         data = self._get(
@@ -191,13 +175,10 @@ class BufferedAsyncRemoteDatalakeClient(SyncRemoteDatalakeClient):
             tags=LogTags.DATALAKE,
         )
 
-    def save(
-        self, collection: str, instances: Union[List[Dict], Dict]
-    ) -> List[Dict]:
-        logger.debug("Saving documents in datalake")
-        instances = instances if isinstance(instances, List) else [instances]
-        if collection not in self._data_buffers:
-            raise InvalidCollectionName(collection)
+    def save(self, records: Records) -> list[dict]:
+        logger.debug("Saving documents in datalake", tags=LogTags.DATALAKE)
+        collection = records["collection"]
+        instances = records["records"]
         buffer = self._data_buffers[collection]
         with self._lock:
             if buffer.should_flush():
@@ -213,12 +194,10 @@ class BufferedAsyncRemoteDatalakeClient(SyncRemoteDatalakeClient):
     def save_dataframe(
         self, dataframe: pd.DataFrame, collection: str = DEFAULT_COLLECTION
     ) -> None:
-        logger.debug("Saving dataframe in datalake")
-        dataframe["timestamp"] = dataframe["timestamp"].apply(
-            lambda x: x.tz_localize(tz="UTC").isoformat()
-        )
+        logger.debug("Saving dataframe in datalake", tags=LogTags.DATALAKE)
+        dataframe = _fix_dataframe_timestamp(dataframe)
         instances = dataframe.to_dict("records")
-        self.save(collection, instances)
+        self.save({"collection": collection, "records": instances})
 
     def _flusher(self):
         while True:
@@ -242,15 +221,15 @@ class BufferedAsyncRemoteDatalakeClient(SyncRemoteDatalakeClient):
                     logger.error("Unable to save documents", exc_info=True)
 
     @retry(SPLIGHT_REQUEST_EXCEPTIONS, tries=3, delay=2, jitter=1)
-    def _send_documents(self, collection: str, docs: List[Dict]) -> List[Dict]:
+    def _send_documents(self, collection: str, docs: list[dict]) -> list[dict]:
         url = self._base_url / f"{self._PREFIX}/write"
-        collection = camelcase(collection)
         data = {
             "collection": collection,
             "records": docs,
         }
         response = self._restclient.post(url, json=data)
-        response.raise_for_status()
+        if response.is_error:
+            raise DatalakeRequestError(response.status_code, response.text)
         return docs
 
 
@@ -288,6 +267,7 @@ class BufferedSyncRemoteDataClient(SyncRemoteDatalakeClient):
             "Initializing buffer with size %s and timeout %s",
             buffer_size,
             buffer_timeout,
+            tags=LogTags.DATALAKE,
         )
         self._data_buffers = {
             "default": DatalakeDocumentBuffer(buffer_size, buffer_timeout),
@@ -301,43 +281,53 @@ class BufferedSyncRemoteDataClient(SyncRemoteDatalakeClient):
             tags=LogTags.DATALAKE,
         )
 
-    def save(
-        self, collection: str, instances: Union[List[Dict], Dict]
-    ) -> List[Dict]:
-        logger.debug("Saving documents in datalake")
-        instances = instances if isinstance(instances, List) else [instances]
-        if collection not in self._data_buffers:
-            raise InvalidCollectionName(collection)
+    def save(self, records: Records) -> list[dict]:
+        logger.debug("Saving documents in datalake", tag=LogTags.DATALAKE)
+        collection = records["collection"]
         buffer = self._data_buffers[collection]
         with self._lock:
-            buffer.add_documents(instances)
+            buffer.add_documents(records["records"])
             if buffer.should_flush():
                 logger.debug(
                     "Flushing datalake buffer with %s elements",
                     len(buffer.data),
+                    tags=LogTags.DATALAKE,
                 )
                 self._send_documents(collection, buffer.data)
                 buffer.reset()
-        return instances
+        return records["records"]
 
     def save_dataframe(
         self, dataframe: pd.DataFrame, collection: str = DEFAULT_COLLECTION
     ) -> None:
-        logger.debug("Saving dataframe in datalake")
-        dataframe["timestamp"] = dataframe["timestamp"].apply(
-            lambda x: x.tz_localize(tz="UTC").isoformat()
-        )
+        logger.debug("Saving dataframe in datalake", tag=LogTags.DATALAKE)
+        # dataframe["timestamp"] = dataframe["timestamp"].apply(
+        #     lambda x: x.tz_localize(tz="UTC").isoformat()
+        # )
+        dataframe = _fix_dataframe_timestamp(dataframe)
         instances = dataframe.to_dict("records")
         self.save(collection, instances)
 
     @retry(SPLIGHT_REQUEST_EXCEPTIONS, tries=3, delay=2, jitter=1)
-    def _send_documents(self, collection: str, docs: List[Dict]) -> List[Dict]:
+    def _send_documents(self, collection: str, docs: list[dict]) -> list[dict]:
         url = self._base_url / f"{self._PREFIX}/write"
-        collection = camelcase(collection)
         data = {
             "collection": collection,
             "records": docs,
         }
         response = self._restclient.post(url, json=data)
-        response.raise_for_status()
+        if response.is_error:
+            raise DatalakeRequestError(response.status_code, response.text)
         return docs
+
+
+def _fix_dataframe_timestamp(df: pd.DataFrame) -> pd.DataFrame:
+    if df["timestamp"][0].tz is None:
+        df["timestamp"] = df["timestamp"].apply(
+            lambda x: x.tz_localize(tz="UTC").strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+        )
+    else:
+        df["timestamp"] = df["timestamp"].apply(
+            lambda x: x.tz_convert(tz="UTC").strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+        )
+    return df
