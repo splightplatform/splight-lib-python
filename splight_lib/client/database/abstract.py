448a95b3c1f8ddf8b021cb8d18cd0865ff535949
marianogaragiola <marianogaragiola@gmail.com>
2024-09-20 09:49:45 -0300

    type hints for database client

--- splight_lib/client/database/abstract.py
+++ splight_lib/client/database/abstract.py
@@ -1,19 +1,18 @@
 from abc import abstractmethod
 from tempfile import NamedTemporaryFile
-from typing import Dict, List, Union
 
 from splight_lib.abstract.client import AbstractClient, QuerySet
 
 
 class AbstractDatabaseClient(AbstractClient):
     @abstractmethod
-    def save(self, resource_name: str, instance: Dict) -> Dict:
+    def save(self, resource_name: str, instance: dict) -> dict:
         pass
 
     @abstractmethod
     def _get(
         self, resource_name: str, first: bool = False, **kwargs
-    ) -> Union[Dict, List[Dict]]:
+    ) -> dict | list[dict]:
         pass
 
     def get(self, resource_name: str, *args, **kwargs) -> QuerySet:
@@ -24,5 +23,5 @@ class AbstractDatabaseClient(AbstractClient):
         pass
 
     @abstractmethod
-    def download(self, instance: Dict) -> NamedTemporaryFile:
+    def download(self, instance: dict) -> NamedTemporaryFile:
         pass
