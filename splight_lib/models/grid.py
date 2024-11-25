3644aea4e89c6e8248a372a109211fa214bf40a8
Franco Zacco <francozacco@gmial.com>
2024-11-25 11:47:00 +0100

    formatting again

--- splight_lib/models/grid.py
+++ splight_lib/models/grid.py
@@ -2,4 +2,5 @@ from splight_lib.models import AssetParams
 from splight_lib.models.database_base import SplightDatabaseBaseModel
 
 
-class Grid(AssetParams, SplightDatabaseBaseModel): ...
+class Grid(AssetParams, SplightDatabaseBaseModel):
+    ...
