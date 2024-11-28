cc3d93803fcb0515a738b1a288682311aff52aa2
Franco Zacco <francozacco@gmial.com>
2024-11-28 17:22:14 +0100

    metadata validation

--- splight_lib/models/metadata.py
+++ splight_lib/models/metadata.py
@@ -3,7 +3,11 @@ from typing import Any
 from pydantic import model_validator
 
 from splight_lib.models.database_base import SplightDatabaseBaseModel
-from splight_lib.models.generic import ValueTypeEnum, cast_value
+from splight_lib.models.generic import (
+    ValueTypeEnum,
+    cast_value,
+    is_empty_str_and_num,
+)
 
 
 class Metadata(SplightDatabaseBaseModel):
@@ -17,8 +21,9 @@ class Metadata(SplightDatabaseBaseModel):
     @model_validator(mode="after")
     def check_value_type(cls, model):
         model.value = (
-            model.value
-            if model.value is None or model.value == ""
+            None
+            if model.value is None
+            or is_empty_str_and_num(model.value, model.type)
             else cast_value(model.value, model.type)
         )
         return model
