9941bea46b31d2f3e3c01030e45dc218c6cd9a53
smartinellimarco <smartinellimarco@icloud.com>
2024-12-27 14:56:24 -0300

    Expose kind models

--- splight_lib/models/__init__.py
+++ splight_lib/models/__init__.py
@@ -7,6 +7,7 @@ from splight_lib.models.asset import (
     AssetRelationship,
 )
 from splight_lib.models.attribute import Attribute
+from splight_lib.models.bus import Bus
 from splight_lib.models.component import (
     Component,
     ComponentObject,
@@ -38,15 +39,27 @@ from splight_lib.models.dashboard import (
     Tab,
 )
 from splight_lib.models.datalake import DataRequest, PipelineStep, Trace
+from splight_lib.models.external_grid import ExternalGrid
 from splight_lib.models.file import File
 from splight_lib.models.function import Function, FunctionItem, QueryFilter
+from splight_lib.models.generator import Generator
+from splight_lib.models.grid import Grid
 from splight_lib.models.hub import HubComponent
 from splight_lib.models.hub_server import HubServer
+from splight_lib.models.inverter import Inverter
+from splight_lib.models.line import Line
 from splight_lib.models.metadata import Metadata
 from splight_lib.models.native import Boolean, Number, String
 from splight_lib.models.secret import Secret
+from splight_lib.models.segment import Segment
 from splight_lib.models.server import Server
+from splight_lib.models.slack_generator import SlackGenerator
+from splight_lib.models.slack_line import SlackLine
 from splight_lib.models.tag import Tag
+from splight_lib.models.three_winding_transformer import (
+    ThreeWindingTransformer,
+)
+from splight_lib.models.transformer import Transformer
 
 __all__ = [
     Action,
@@ -91,6 +104,15 @@ __all__ = [
     Metadata,
     Number,
     PipelineStep,
+    Bus,
+    ExternalGrid,
+    Generator,
+    Grid,
+    Inverter,
+    SlackGenerator,
+    SlackLine,
+    ThreeWindingTransformer,
+    Transformer,
     RoutineEvaluation,
     Secret,
     Server,
