b4c2ff292c7aad3909975ef341610e35bfd2193b
Franco Zacco <francozacco@gmial.com>
2025-03-14 15:17:16 +0100

    adds load kind

---
+++ splight_lib/models/load.py
@@ -0,0 +1,20 @@
+from splight_lib.models import AssetParams, AssetRelationship, Attribute
+from splight_lib.models.database_base import SplightDatabaseBaseModel
+from splight_lib.settings import SplightAPIVersion, api_settings
+from pydantic import model_validator
+from typing import Any
+
+
+class Load(AssetParams, SplightDatabaseBaseModel):
+    active_power: Attribute | None = None
+    reactive_power: Attribute | None = None
+    switch_status: Attribute | None = None
+    bus: AssetRelationship | None = None
+    grid: AssetRelationship | None = None
+
+    @model_validator(mode="after")
+    @classmethod
+    def check_api_version(cls, data: Any) -> Any:
+        if api_settings.API_VERSION == SplightAPIVersion.V3:
+            raise NotImplementedError("Load kind is not supported in API v3.")
+        return data
