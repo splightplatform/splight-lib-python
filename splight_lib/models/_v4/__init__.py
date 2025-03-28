635951283c79529fef3b2c363d0e75911143664f
smartinellimarco <smartinellimarco@icloud.com>
2025-03-28 16:18:55 -0300

    Add import blocker

--- splight_lib/models/_v4/__init__.py
+++ splight_lib/models/_v4/__init__.py
@@ -1,1 +1,9 @@
+from splight_lib.settings import SplightAPIVersion, api_settings
+
+if api_settings.API_VERSION != SplightAPIVersion.V4:
+    raise ImportError(
+        f"Unable to import models from this module when API_VERSION is not set to '{SplightAPIVersion.V4}'"
+    )
+
+
 __all__ = []
